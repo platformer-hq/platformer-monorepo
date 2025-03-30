6d3e24903241a0fd1ed2f8c5f462b36f3add1b87
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-31 01:19:04 +0500

    feat(solid-utils): implement package

---
+++ packages/solid-utils/src/index.ts
@@ -0,0 +1,8 @@
+export * from './accessor.js';
+export * from './composeHandlers.js';
+export * from './omitProps.js';
+export * from './pickChildren.js';
+export * from './pickProps.js';
+export * from './signalsToRecord.js';
+
+export * from '@solid-primitives/utils';
