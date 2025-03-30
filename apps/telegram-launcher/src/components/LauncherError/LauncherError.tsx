f5a99e89d781f80999bcd231254ae96ed9f4b045
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-31 02:21:54 +0500

    chore(launcher): remove LauncherError page

--- apps/telegram-launcher/src/components/LauncherError/LauncherError.tsx
+++
@@ -1,7 +0,0 @@
-import { StatusPage, type StatusPageProps } from '@/components/StatusPage/StatusPage.js';
-
-import image from './sad.png?process';
-
-export function LauncherError(props: Pick<StatusPageProps, 'title' | 'subtitle'>) {
-  return <StatusPage {...props} image={image}/>;
-}
