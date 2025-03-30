a8a6a33cd2ed4cacbd9af83afa2e71a45731f7b0
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-31 02:10:10 +0500

    feat(launcher): implement MainProvider

---
+++ apps/telegram-launcher/src/providers/MainProvider.tsx
@@ -0,0 +1,23 @@
+import type { Platform } from '@telegram-apps/sdk-solid';
+import { createContext, type FlowProps, useContext } from 'solid-js';
+import { pickProps } from 'solid-utils';
+
+const Context = createContext<{
+  platform: Platform;
+}>();
+
+export function useMainContext() {
+  const context = useContext(Context);
+  if (!context) {
+    throw new Error('Used outside context');
+  }
+  return context;
+}
+
+export function MainProvider(props: FlowProps<{ platform: Platform }>) {
+  return (
+    <Context.Provider value={pickProps(props, 'platform')}>
+      {props.children}
+    </Context.Provider>
+  );
+}
