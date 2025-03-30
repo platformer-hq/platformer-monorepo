ebfef2584e645737f15a52110a6af3648efbdb0e
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-30 22:20:11 +0500

    chore(shared): export formatViewportCssVar and createLogger

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,4 +3,6 @@ export * from './execution/types.js';
 export * from './storage/auth-token.js';
 export * from './storage/storage.js';
 export * from './camelToKebab.js';
+export * from './createLogger.js';
+export * from './formatViewportCssVar.js';
 export * from './lazy.js';
