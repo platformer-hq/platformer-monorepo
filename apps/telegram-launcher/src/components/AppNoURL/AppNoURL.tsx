68fcfc3566bfc767df294128a08dfe01dfb0ab92
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-31 12:35:46 +0500

    refactor(launcher): actualize AppNoURL

--- apps/telegram-launcher/src/components/AppNoURL/AppNoURL.tsx
+++ apps/telegram-launcher/src/components/AppNoURL/AppNoURL.tsx
@@ -1,13 +1,10 @@
 import { StatusPage } from '@/components/StatusPage/StatusPage.js';
 
-import image from './nothing-here.png?process';
-
 export function AppNoURL() {
   return (
     <StatusPage
-      image={image}
       title="Nothing here"
-      subtitle="The application is inaccessible on your device"
+      text="The application is inaccessible on your device"
     />
   );
 }
