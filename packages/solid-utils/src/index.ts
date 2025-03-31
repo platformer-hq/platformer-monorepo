1e0a2f2c0dee8d85bb20c67a3fc5557cd277d7e9
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-31 14:55:33 +0500

    feat(solid-utils): implement splitExecutionTuple

--- packages/solid-utils/src/index.ts
+++ packages/solid-utils/src/index.ts
@@ -4,5 +4,6 @@ export * from './omitProps.js';
 export * from './pickChildren.js';
 export * from './pickProps.js';
 export * from './signalsToRecord.js';
+export * from './splitExecutionTuple.js';
 
 export * from '@solid-primitives/utils';
