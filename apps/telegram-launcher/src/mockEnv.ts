9d30b36592e0f2391b671df7914b7d85a396f1f8
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-01 00:59:15 +0500

    refactor(launcher): fix mock launch params

--- apps/telegram-launcher/src/mockEnv.ts
+++ apps/telegram-launcher/src/mockEnv.ts
@@ -20,12 +20,7 @@ if (import.meta.env.DEV) {
       subtitle_text_color: '#708499',
       text_color: '#f5f5f5',
     } as const;
-    const noInsets = {
-      left: 0,
-      top: 0,
-      bottom: 0,
-      right: 0,
-    } as const;
+    const noInsets = { left: 0, top: 0, bottom: 0, right: 0 } as const;
 
     mockTelegramEnv({
       onEvent(e) {
@@ -47,22 +42,21 @@ if (import.meta.env.DEV) {
           return emitEvent('safe_area_changed', noInsets);
         }
       },
-      launchParams: 'tgWebAppData=user%3D%257B%2522id%2522%253A279058397%252C%2522first_name%2522%253A%2522Vladislav%2522%252C%2522last_name%2522%253A%2522Kibenko%2522%252C%2522username%2522%253A%2522vdkfrost%2522%252C%2522language_code%2522%253A%2522ru%2522%252C%2522is_premium%2522%253Atrue%252C%2522allows_write_to_pm%2522%253Atrue%252C%2522photo_url%2522%253A%2522https%253A%255C%252F%255C%252Ft.me%255C%252Fi%255C%252Fuserpic%255C%252F320%255C%252F4FPEE4tmP3ATHa57u6MqTDih13LTOiMoKoLDRG4PnSA.svg%2522%257D%26chat_instance%3D-765556968080613175%26chat_type%3Dsender%26auth_date%3D1741854951%26signature%3DnRsZLYSbwxKpKbjAFWVAoQ1ccdTqXQ00-Em21gj3i3ZiVIeMfWoJAcLZ0hyiAtDnCjJUBzSbe-sVSV8DnYppDg%26hash%3D496de5ac0b9a1977c6fa7067784aa4691c4cec62369bbe344b28899a74b9adbb&tgWebAppVersion=8.0&tgWebAppPlatform=tdesktop&tgWebAppThemeParams=%7B%22accent_text_color%22%3A%22%236ab2f2%22%2C%22bg_color%22%3A%22%2317212b%22%2C%22bottom_bar_bg_color%22%3A%22%2317212b%22%2C%22button_color%22%3A%22%235288c1%22%2C%22button_text_color%22%3A%22%23ffffff%22%2C%22destructive_text_color%22%3A%22%23ec3942%22%2C%22header_bg_color%22%3A%22%2317212b%22%2C%22hint_color%22%3A%22%23708499%22%2C%22link_color%22%3A%22%236ab3f3%22%2C%22secondary_bg_color%22%3A%22%23232e3c%22%2C%22section_bg_color%22%3A%22%2317212b%22%2C%22section_header_text_color%22%3A%22%236ab3f3%22%2C%22section_separator_color%22%3A%22%23111921%22%2C%22subtitle_text_color%22%3A%22%23708499%22%2C%22text_color%22%3A%22%23f5f5f5%22%7D',
-      //   {
-      //   tgWebAppThemeParams: themeParams,
-      //   tgWebAppData: new URLSearchParams([
-      //     ['user', JSON.stringify({
-      //       id: 1,
-      //       first_name: 'Pavel',
-      //     })],
-      //     ['hash', ''],
-      //     ['signature', ''],
-      //     ['auth_date', Date.now().toString()],
-      //   ]).toString(),
-      //   tgWebAppStartParam: 'debug',
-      //   tgWebAppVersion: '8',
-      //   tgWebAppPlatform: 'tdesktop',
-      // },
+      launchParams: {
+        tgWebAppThemeParams: themeParams,
+        tgWebAppData: new URLSearchParams([
+          ['user', JSON.stringify({
+            id: 1,
+            first_name: 'Pavel',
+          })],
+          ['hash', ''],
+          ['signature', ''],
+          ['auth_date', Date.now().toString()],
+        ]).toString(),
+        tgWebAppStartParam: 'debug',
+        tgWebAppVersion: '8',
+        tgWebAppPlatform: 'tdesktop',
+      },
     });
 
     console.info(
