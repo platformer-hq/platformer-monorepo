d0b4d79f1538bce5a073866b1796e7a90a637591
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-03-31 12:37:42 +0500

    refactor(launcher): refactor index file

--- apps/telegram-launcher/src/index.tsx
+++ apps/telegram-launcher/src/index.tsx
@@ -1,9 +1,36 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-solid';
 
 import { Root } from '@/components/Root/Root.js';
+import { init } from '@/init.js';
 
 import './index.scss';
 import './mockEnv.js';
 
-render(() => <Root/>, document.getElementById('root')!);
+const { tgWebAppPlatform: platform, tgWebAppStartParam: startParam } = retrieveLaunchParams();
+const debug = (startParam || '').includes('platformer_debug') || import.meta.env.DEV;
+
+init({
+  debug,
+  eruda: debug && ['ios', 'android'].includes(platform),
+  mockForMacOS: platform === 'macos',
+  sentry: {
+    dsn: 'https://8888815a88eb8e06bd1ac55195df9ab0@o992980.ingest.us.sentry.io/4508812774473728',
+    environment: import.meta.env.MODE,
+  },
+})
+  .then(({ initialColors }) => {
+    render(() => (
+      <Root
+        debug={debug}
+        platform={platform}
+        // platform="ios"
+        initialColors={initialColors}
+      />
+    ), document.getElementById('app')!);
+  })
+  .catch(error => {
+    // TODO: Should properly handle the error.
+    console.error(error);
+  });
