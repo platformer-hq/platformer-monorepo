33a1eab27869ab007319cefd039f5c1f34992b08
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-02 00:12:56 +0500

    refactor(launcher): refactor Root component

--- apps/telegram-launcher/src/components/Root/Root.tsx
+++ apps/telegram-launcher/src/components/Root/Root.tsx
@@ -1,86 +1,26 @@
-import { createMemo, createSignal, ErrorBoundary, Show } from 'solid-js';
-import {
-  retrieveRawLaunchParams,
-  retrieveRawInitData,
-  transformQueryUsing,
-  type Platform,
-  type MiniAppHeaderColor,
-  type BackgroundColor,
-  type BottomBarColor,
+import { createMemo, createSignal, ErrorBoundary, Match, Show, Switch } from 'solid-js';
+import type {
+  Platform,
+  MiniAppHeaderColor,
+  BackgroundColor,
+  BottomBarColor,
 } from '@telegram-apps/sdk-solid';
-import {
-  instance,
-  looseObject,
-  optional,
-  pipe,
-  string,
-  transform,
-  parse,
-  ValiError,
-  union,
-} from 'valibot';
 import { pickProps, accessor } from 'solid-utils';
+import { GqlProvider } from 'shared';
 
-import { BootstrapApp } from '@/components/BootstrapApp/BootstrapApp.js';
-import { RootErrorBoundary } from '@/components/RootErrorBoundary/RootErrorBoundary.js';
-import { positiveIntFromStr } from '@/validation/positiveIntFromStr.js';
-import { splitExecutionTuple } from '@/helpers/splitExecutionTuple.js';
+import { AppLoader } from '@/components/AppLoader/AppLoader.js';
 import { ErrorStatusPage } from '@/components/ErrorStatusPage/ErrorStatusPage.js';
 import { StatusPage } from '@/components/StatusPage/StatusPage.js';
 import { MainProvider, useMainContext } from '@/providers/MainProvider.js';
+import {
+  TypedErrorStatusPage,
+  type TypedErrorStatusPageError,
+} from '@/components/TypedErrorStatusPage/TypedErrorStatusPage.js';
 
-import { computeFallbackURL, secureInitData, secureLaunchParams } from './utils.js';
+import { useLauncherOptions } from './useLauncherOptions.js';
+import { computeFallbackURL } from './utils.js';
 
 import './Root.scss';
-
-function useLauncherOptions() {
-  return splitExecutionTuple<{
-    appID: number;
-    apiBaseURL: string;
-    fallbackURL?: Maybe<string>;
-    initTimeout: number;
-    loadTimeout: number;
-  }, string>(() => {
-    try {
-      const argsObject = parse(
-        pipe(
-          union([instance(URLSearchParams), string()]),
-          transformQueryUsing(
-            looseObject({
-              app_id:
-                optional(positiveIntFromStr(), '1'),
-              // positiveIntFromStr(),
-              api_base_url: optional(
-                pipe(
-                  string(),
-                  transform(v => new URL(v, window.location.origin).toString()),
-                ),
-                'https://mini-apps.store/gql',
-              ),
-              fallback_url: optional(string()),
-              init_timeout: optional(positiveIntFromStr(), '5000'),
-              load_timeout: optional(positiveIntFromStr(), '1000000'),
-            }),
-          ),
-        ),
-        new URLSearchParams(
-          // Telegram API has a bug replacing & with &amp; for some reason. We are replacing it
-          // back.
-          window.location.search.replace(/&amp;/g, '&'),
-        ),
-      );
-      return [true, {
-        appID: argsObject.app_id,
-        apiBaseURL: argsObject.api_base_url,
-        fallbackURL: argsObject.fallback_url,
-        initTimeout: argsObject.init_timeout,
-        loadTimeout: argsObject.load_timeout,
-      }];
-    } catch (e) {
-      return [false, (e as ValiError<never>).message];
-    }
-  });
-}
 
 interface InnerProps {
   debug: boolean;
@@ -89,6 +29,8 @@ interface InnerProps {
     background: BackgroundColor,
     bottomBar: BottomBarColor
   ];
+  rawInitData?: string;
+  rawLaunchParams: string;
 }
 
 interface RootProps extends InnerProps {
@@ -99,11 +41,7 @@ function Inner(props: InnerProps) {
   const [$options, $error] = useLauncherOptions();
   const context = useMainContext();
   const $platform = accessor(context, 'platform');
-
-  // Wait for the bootstrapper to load.
-  const [$bootstrapperReady, setBootstrapperReady] = createSignal(false);
-
-  const rawInitData = retrieveRawInitData() || '';
+  const [$loaderReady, setLoaderReady] = createSignal(false);
 
   return (
     <main
@@ -117,53 +55,80 @@ function Inner(props: InnerProps) {
         fallback={<ErrorStatusPage title="Configuration is invalid" text={$error()}/>}
       >
         {$opts => (
-          <Show
-            when={
-              // We are sanitizing the hash property for security purposes, so Platformer could not
-              // use this init data to impersonate user. Instead, Platformer uses the "signature"
-              // property allowing third parties to validate the init data.
-              secureInitData(rawInitData)
-            }
-            fallback={
-              <ErrorStatusPage
-                title="Init data is missing"
-                text="For some reason, init data is missing. It is the most likely that the application was launched improperly"
-              />
-            }
-          >
-            {$securedInitData => {
-              const rawLaunchParams = retrieveRawLaunchParams();
-              const $securedLaunchParams = createMemo(() => {
-                // Here we do the same thing as we did with the init data - we secure it by
-                // replacing exposed init data with the secured one.
-                return secureLaunchParams(rawLaunchParams, $securedInitData());
-              });
+          <GqlProvider endpoint={$opts().apiBaseURL}>
+            <Show
+              when={props.rawInitData}
+              fallback={
+                <ErrorStatusPage
+                  title="Init data is missing"
+                  text="For some reason, init data is missing. It is the most likely that the application was launched improperly"
+                />
+              }
+            >
+              {$rawInitData => {
+                // Error occurred during application loading.
+                const [$error, setError] = createSignal<TypedErrorStatusPageError>();
 
-              // Compute fallback URL in case something went wrong with Platformer.
-              const $fallbackURL = createMemo(() => {
-                const { fallbackURL } = $opts();
-                return fallbackURL ? computeFallbackURL(fallbackURL, rawLaunchParams) : undefined;
-              });
+                // Compute fallback URL in case something went wrong with Platformer.
+                const $fallbackURL = createMemo(() => {
+                  const { fallbackURL } = $opts();
+                  return fallbackURL
+                    ? computeFallbackURL(fallbackURL, props.rawLaunchParams)
+                    : undefined;
+                });
+                const $securedRawInitData = createMemo(() => {
+                  // We are sanitizing the hash property for security purposes, so Platformer could
+                  // not use this init data to impersonate user. Instead, Platformer uses the
+                  // "signature" property allowing third parties to validate the init data.
+                  const query = new URLSearchParams($rawInitData());
+                  query.set('hash', '');
+                  return query.toString();
+                });
+                const $securedRawLaunchParams = createMemo(() => {
+                  // Here we do the same thing as we did with the init data - we secure it by
+                  // replacing exposed init data with the secured one.
+                  const query = new URLSearchParams(props.rawLaunchParams);
+                  query.set('tgWebAppData', $securedRawInitData());
+                  return query.toString();
+                });
 
-              return (
-                <>
-                  <Show when={!$bootstrapperReady()}>
-                    <StatusPage state="loading"/>
-                  </Show>
-                  <BootstrapApp
-                    {...$opts()}
-                    fallbackURL={$fallbackURL()}
-                    onReady={() => {
-                      setBootstrapperReady(true);
-                    }}
-                    rawLaunchParams={rawLaunchParams}
-                    securedInitData={$securedInitData()}
-                    securedLaunchParams={$securedLaunchParams()}
-                  />
-                </>
-              );
-            }}
-          </Show>
+                return (
+                  <Switch>
+                    <Match when={$error()}>
+                      {$err => <TypedErrorStatusPage error={$err()}/>}
+                    </Match>
+                    <Match when>
+                      <Show when={!$loaderReady()}>
+                        <StatusPage state="loading"/>
+                      </Show>
+                      <AppLoader
+                        {...pickProps(
+                          $opts(),
+                          ['apiBaseURL', 'appID', 'initTimeout', 'loadTimeout'],
+                        )}
+                        {...pickProps(props, ['rawLaunchParams'])}
+                        fallbackURL={$fallbackURL()}
+                        securedRawInitData={$securedRawInitData()}
+                        securedRawLaunchParams={$securedRawLaunchParams()}
+                        onError={(error, fallbackURL) => {
+                          fallbackURL && console.error('Fallback URL failed to load:', fallbackURL);
+                          setError(error);
+                          setLoaderReady(true);
+                        }}
+                        onReady={(fallbackURL) => {
+                          fallbackURL && console.warn(
+                            'Platformer failed to load. Used fallback:',
+                            fallbackURL,
+                          );
+                          setLoaderReady(true);
+                        }}
+                      />
+                    </Match>
+                  </Switch>
+                );
+              }}
+            </Show>
+          </GqlProvider>
         )}
       </Show>
     </main>
@@ -172,8 +137,8 @@ function Inner(props: InnerProps) {
 
 export function Root(props: RootProps) {
   return (
-    <MainProvider {...pickProps(props, 'platform')}>
-      <ErrorBoundary fallback={RootErrorBoundary}>
+    <MainProvider {...pickProps(props, ['platform'])}>
+      <ErrorBoundary fallback={error => <TypedErrorStatusPage error={['unknown', error]}/>}>
         <Inner {...props}/>
       </ErrorBoundary>
     </MainProvider>
