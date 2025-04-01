f5a5ef31568810a789750800b40e1becdb31f4be
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-02 00:11:49 +0500

    refactor(launcher): move useLauncherOptions to a separate file

---
+++ apps/telegram-launcher/src/components/Root/useLauncherOptions.ts
@@ -0,0 +1,62 @@
+import {
+  instance,
+  looseObject,
+  optional,
+  parse,
+  pipe,
+  string,
+  transform,
+  union,
+  ValiError,
+} from 'valibot';
+import { transformQueryUsing } from '@telegram-apps/sdk-solid';
+import { splitExecutionTuple } from 'solid-utils';
+
+import { positiveIntFromStr } from '@/validation/positiveIntFromStr.js';
+
+export function useLauncherOptions() {
+  return splitExecutionTuple<{
+    appID: number;
+    apiBaseURL: string;
+    fallbackURL?: Maybe<string>;
+    initTimeout: number;
+    loadTimeout: number;
+  }, string>(() => {
+    try {
+      const argsObject = parse(
+        pipe(
+          union([instance(URLSearchParams), string()]),
+          transformQueryUsing(
+            looseObject({
+              app_id: positiveIntFromStr(),
+              api_base_url: optional(
+                pipe(
+                  string(),
+                  transform(v => new URL(v, window.location.origin).toString()),
+                ),
+                'https://mini-apps.store/gql',
+              ),
+              fallback_url: optional(string()),
+              init_timeout: optional(positiveIntFromStr(), '5000'),
+              load_timeout: optional(positiveIntFromStr(), '10000'),
+            }),
+          ),
+        ),
+        new URLSearchParams(
+          // Telegram API has a bug replacing & with &amp; for some reason. We are replacing it
+          // back.
+          window.location.search.replace(/&amp;/g, '&'),
+        ),
+      );
+      return [true, {
+        appID: argsObject.app_id,
+        apiBaseURL: argsObject.api_base_url,
+        fallbackURL: argsObject.fallback_url,
+        initTimeout: argsObject.init_timeout,
+        loadTimeout: argsObject.load_timeout,
+      }];
+    } catch (e) {
+      return [false, (e as ValiError<never>).message];
+    }
+  });
+}
