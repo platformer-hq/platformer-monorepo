350bdc304e4e01c841b0fb9eda7d3c1f6b16eb4a
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-01 23:59:21 +0500

    refactor(launcher): actualize TypedErrorStatusPage

--- apps/telegram-launcher/src/components/TypedErrorStatusPage/TypedErrorStatusPage.tsx
+++ apps/telegram-launcher/src/components/TypedErrorStatusPage/TypedErrorStatusPage.tsx
@@ -1,10 +1,11 @@
-import { For, Match, Switch } from 'solid-js';
+import { createMemo, Match, Show, Switch } from 'solid-js';
+import { type UseGqlError, GraphQLError } from 'solid-gql';
 
-import type { GqlRequestError } from '@/api/gqlRequest.js';
 import { ErrorStatusPage } from '@/components/ErrorStatusPage/ErrorStatusPage.jsx';
+import { is, looseObject, string } from 'valibot';
 
 export type TypedErrorStatusPageError =
-  | GqlRequestError
+  | UseGqlError
   | ['iframe', timeout?: boolean]
   | ['unknown', unknown];
 
@@ -12,56 +13,50 @@ export type TypedErrorStatusPageError =
  * Used to handle all kinds of errors.
  */
 export function TypedErrorStatusPage(props: { error: TypedErrorStatusPageError }) {
-  const networkErrTitle = 'Network error';
-  const oopsTitle = 'Oops!';
-
-  function withError<T>(fn: (err: TypedErrorStatusPageError) => T) {
-    return () => fn(props.error);
+  function withError<T>(
+    fn: (err: ['gql', UseGqlError] | ['iframe', timeout?: boolean] | ['unknown', unknown]) => T,
+  ) {
+    return () => {
+      const { error } = props;
+      return fn(Array.isArray(error) ? error : ['gql', error]);
+    };
   }
 
-  const whenFetch = withError(e => e[0] === 'fetch' ? e[1] : false);
-  const whenGql = withError(e => e[0] === 'gql' ? e[1] : false);
-  const whenHttp = withError(e => e[0] === 'http' ? [e[1], e[2]] as [number, string] : false);
-  const whenInvalidData = withError(e => e[0] === 'invalid-data' ? e[1] : false);
+  const whenGql = withError(e => {
+    return e[0] === 'gql' && GraphQLError.is(e[1]) ? e[1] : false;
+  });
   const whenIframe = withError(e => e[0] === 'iframe' ? [e[1]] : false);
-  const whenExecution = withError(e => e[0] === 'execution' ? e[1] : false);
-  const whenUnknown = withError(e => e[0] === 'unknown' ? e[1] : false);
+  const whenUnknown = withError(e => {
+    return e[0] === 'unknown' || (e[0] === 'gql' && !GraphQLError.is(e[0])) ? e[1] : false;
+  });
+  const title = 'Oops!';
 
   return (
     <Switch>
-      <Match when={whenFetch()}>
-        <ErrorStatusPage
-          title={networkErrTitle}
-          text="Unable to send request to the server. The server is unreachable"
-        />
-      </Match>
       <Match when={whenGql()}>
-        {$errors => (
-          <ErrorStatusPage
-            title={oopsTitle}
-            text={
-              <>
-                Server returned errors:{' '}
-                <For each={$errors()}>
-                  {($error, idx) => (
-                    <>
-                      {idx() ? ', ' : ''}{$error.message}&nbsp;
-                      <b>({$error.data.code})</b>
-                    </>
-                  )}
-                </For>
-              </>
-            }
-          />
-        )}
-      </Match>
-      <Match when={whenHttp()}>
-        {$error => (
-          <ErrorStatusPage
-            title={networkErrTitle}
-            text={`Server responded with status ${$error()[0]}: ${$error()[1]}`}
-          />
-        )}
+        {$error => {
+          const $code = createMemo(() => {
+            const { extensions } = $error();
+            return is(looseObject({ errorData: looseObject({ code: string() }) }), extensions)
+              ? extensions.errorData.code
+              : false;
+          });
+
+          return (
+            <ErrorStatusPage
+              title={title}
+              text={
+                <>
+                  Server returned error:{' '}
+                  {$error().message}
+                  <Show when={$code()}>
+                    &nbsp;<b>({$code()})</b>
+                  </Show>
+                </>
+              }
+            />
+          );
+        }}
       </Match>
       <Match when={whenUnknown()}>
         {$error => {
@@ -69,30 +64,14 @@ export function TypedErrorStatusPage(props: { error: TypedErrorStatusPageError }
             const error = $error();
             return `Unknown error occurred${error instanceof Error ? `: ${error.message}` : ''}`;
           };
-          return <ErrorStatusPage title={oopsTitle} text={message()}/>;
+          return <ErrorStatusPage title={title} text={message()}/>;
         }}
-      </Match>
-      <Match when={whenInvalidData()}>
-        {$error => (
-          <ErrorStatusPage
-            title={oopsTitle}
-            text={`Server returned unexpected response: ${$error().message}`}
-          />
-        )}
       </Match>
       <Match when={whenIframe()}>
         {$tuple => (
           <ErrorStatusPage
-            title={oopsTitle}
+            title={title}
             text={`Application failed to load due to ${$tuple()[0] ? 'timeout' : 'unknown reason'}`}
-          />
-        )}
-      </Match>
-      <Match when={whenExecution()}>
-        {$error => (
-          <ErrorStatusPage
-            title={oopsTitle}
-            text={`Application failed to load. ${$error().message}`}
           />
         )}
       </Match>
