d9c95c9fdc4f3342fe332c89da167c61e64d3b97
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-01 23:59:32 +0500

    chore(launcher): remove RootErrorBoundary

--- apps/telegram-launcher/src/components/RootErrorBoundary/RootErrorBoundary.tsx
+++
@@ -1,8 +0,0 @@
-import { TypedErrorStatusPage } from '@/components/TypedErrorStatusPage/TypedErrorStatusPage.js';
-
-/**
- * Error boundary used in the application's root.
- */
-export function RootErrorBoundary(error: unknown) {
-  return <TypedErrorStatusPage error={['unknown', error]}/>;
-}
