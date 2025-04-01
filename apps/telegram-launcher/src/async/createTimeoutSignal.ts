946c80433eab37e1cb6361d77d8522a8f400d51f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-01 23:58:30 +0500

    refactor(launcher): reuse utils from solid-utils

--- apps/telegram-launcher/src/async/createTimeoutSignal.ts
+++ apps/telegram-launcher/src/async/createTimeoutSignal.ts
@@ -1,15 +1,16 @@
 import { type Accessor, createEffect, createSignal, onCleanup } from 'solid-js';
 import { TimeoutError } from 'better-promises';
+import { type MaybeAccessor, access } from 'solid-utils';
 
 /**
  * Creates a new self-cleaning-up abort signal.
  * @param timeout - timeout duration
  */
-export function createTimeoutSignal(timeout: number | Accessor<number>): Accessor<AbortSignal> {
+export function createTimeoutSignal(timeout: MaybeAccessor<number>): Accessor<AbortSignal> {
   const [$controller, setController] = createSignal(new AbortController());
 
   createEffect(() => {
-    const ms = typeof timeout === 'function' ? timeout() : timeout;
+    const ms = access(timeout);
     const controller = $controller();
     const timeoutID = setTimeout(() => {
       controller.abort(new TimeoutError(ms));
