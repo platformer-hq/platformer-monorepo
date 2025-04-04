149db9199c533aafe060eb1e51294c4aa654045f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-05 01:10:12 +0500

    chore(utils): actualize casings file name

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 export * from './css/bem.js';
 export * from './css/cn.js';
 export * from './storage.js';
-export * from './camelToKebab.js';
+export * from './casing.js';
 export * from './createLogger.js';
 export * from './lazy.js';
