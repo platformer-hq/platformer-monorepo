1678c07c79ba53023b57f9aa5c83f155460bb4a1
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-08 21:06:15 +0500

    fix(shared): use a higher auth token timeout threshold in getAuthTokenFromStorage

--- packages/shared/src/auth-token.ts
+++ packages/shared/src/auth-token.ts
@@ -31,9 +31,10 @@ const AuthToken = looseObject({
 function getAuthTokenFromStorage(): { token: string, expiresAt: Date } | undefined {
   try {
     const json = parse(AuthToken, JSON.parse(getStorageItem(STORAGE_KEY)));
-    // Check if the token hasn't expired. We consider it expired if less than 30 seconds left
-    // until the expiration date.
-    return Date.now() + 30000 <= json.expiresAt.getTime() ? json : undefined;
+    // Check if the token hasn't expired. We consider it expired if less than 10 minutes left
+    // until the expiration date. We use such a big interval to make sure it will be enough for
+    // the current user usage session.
+    return Date.now() + 600_000 <= json.expiresAt.getTime() ? json : undefined;
   } catch {
   }
 }
