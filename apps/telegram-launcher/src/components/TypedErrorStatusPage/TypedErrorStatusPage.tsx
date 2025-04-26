3bb1b0ce6de6d5f75edd2d05b3904dae3280239c
qbnk <wolfram.deus@gmail.com>
2025-04-26 20:08:26 +0300

    feat(launcher/TypedErrorStatusPage): localize errors

--- apps/telegram-launcher/src/components/TypedErrorStatusPage/TypedErrorStatusPage.tsx
+++ apps/telegram-launcher/src/components/TypedErrorStatusPage/TypedErrorStatusPage.tsx
@@ -1,15 +1,34 @@
 import { createMemo, Match, Show, Switch } from 'solid-js';
+import { resolveTemplate, translator } from '@solid-primitives/i18n';
 import { type UseGqlError, GraphQLError } from 'solid-gql';
 import { TypographyIos } from 'ui';
+import { is, looseObject, string } from 'valibot';
+import { retrieveLaunchParams } from '@telegram-apps/sdk-solid';
 
 import { ErrorStatusPage } from '@/components/ErrorStatusPage/ErrorStatusPage.jsx';
-import { is, looseObject, string } from 'valibot';
 
 export type TypedErrorStatusPageError =
   | UseGqlError
   | ['init', timeout: number]
   | ['iframe', timeout?: boolean]
   | ['unknown', unknown];
+
+const translations = {
+  en: {
+    apiTimeout: 'Couldn\'t get information about the app (timed out {{time}}ms)',
+    appUnknownError: 'An unknown error occurred while loading the application',
+    loadTimeout: 'The app took too long to load',
+    title: 'Something went wrong',
+    unknownError: 'Unknown error occurred{{ error }}',
+  },
+  ru: {
+    apiTimeout: 'Не удалось получить информацию о приложении (тайм-аут {{time}}мс)',
+    appUnknownError: 'Произошла неизвестная ошибка при загрузке приложения',
+    loadTimeout: 'Загрузка приложения оказалась слишком долгой',
+    title: 'Что-то пошло не так',
+    unknownError: 'Произошла неизвестная ошибка{{ error }}',
+  },
+};
 
 /**
  * Used to handle all kinds of errors.
@@ -30,19 +49,23 @@ export function TypedErrorStatusPage(props: { error: TypedErrorStatusPageError }
     };
   }
 
-  const whenGql = withError(e => {
-    return e[0] === 'gql' && GraphQLError.is(e[1]) ? e[1] : false;
-  });
-  const whenIframe = withError(e => e[0] === 'iframe' ? [e[1]] : false);
-  const whenUnknown = withError(e => {
-    return e[0] === 'unknown' || (e[0] === 'gql' && !GraphQLError.is(e[0])) ? e[1] : false;
-  });
-  const whenInit = withError(e => e[0] === 'init' ? e[1] : false);
-  const title = 'Oops!';
+  let locale: 'en' | 'ru' = 'en';
+  try {
+    const { language_code: lang } = (retrieveLaunchParams().tgWebAppData || {}).user || {};
+    if (lang === 'ru' || lang === 'en') {
+      locale = lang;
+    }
+  } catch {
+  }
+  const t = translator(() => translations[locale], resolveTemplate);
+  const title = t('title');
 
   return (
     <Switch>
-      <Match when={whenGql()}>
+      {/* GraphQL error */}
+      <Match when={withError(e => {
+        return e[0] === 'gql' && GraphQLError.is(e[1]) ? e[1] : false;
+      })()}>
         {$error => {
           const $code = createMemo(() => {
             const { extensions } = $error();
@@ -70,30 +93,34 @@ export function TypedErrorStatusPage(props: { error: TypedErrorStatusPageError }
           );
         }}
       </Match>
-      <Match when={whenUnknown()}>
+
+      {/* Init error */}
+      <Match when={withError(e => e[0] === 'init' ? e[1] : false)()}>
+        {$timeout => (
+          <ErrorStatusPage title={title} text={t('apiTimeout', { time: $timeout() })}/>
+        )}
+      </Match>
+
+      {/* Application error */}
+      <Match when={withError(e => e[0] === 'iframe' ? [e[1]] : false)()}>
+        {$tuple => (
+          <ErrorStatusPage title={title} text={t($tuple()[0] ? 'loadTimeout' : 'appUnknownError')}/>
+        )}
+      </Match>
+
+      {/* Unknown error */}
+      <Match
+        when={withError(e => {
+          return e[0] === 'unknown' || (e[0] === 'gql' && !GraphQLError.is(e[0])) ? e[1] : false;
+        })()}
+      >
         {$error => {
           const message = () => {
             const error = $error();
-            return `Unknown error occurred${error instanceof Error ? `: ${error.message}` : ''}`;
+            return t('unknownError', { error: error instanceof Error ? `: ${error.message}` : '' });
           };
           return <ErrorStatusPage title={title} text={message()}/>;
         }}
-      </Match>
-      <Match when={whenInit()}>
-        {$timeout => (
-          <ErrorStatusPage
-            title={title}
-            text={`Application failed to load due to Platformer not responding in time. Timeout ${$timeout()}ms reached.`}
-          />
-        )}
-      </Match>
-      <Match when={whenIframe()}>
-        {$tuple => (
-          <ErrorStatusPage
-            title={title}
-            text={`Application failed to load due to ${$tuple()[0] ? 'timeout' : 'unknown reason'}`}
-          />
-        )}
       </Match>
     </Switch>
   );
