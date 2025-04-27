a5be476ad21cb4185b8968cbda572eb02389a848
qbnk <wolfram.deus@gmail.com>
2025-04-27 17:42:00 +0300

    refactor(launcher/ErrorStatusPage): remove component

--- apps/telegram-launcher/src/components/ErrorStatusPage/ErrorStatusPage.tsx
+++
@@ -1,5 +0,0 @@
-import { StatusPage, type StatusPageProps } from '@/components/StatusPage/StatusPage.js';
-
-export function ErrorStatusPage(props: Pick<StatusPageProps, 'title' | 'text'>) {
-  return <StatusPage state="error" {...props} />;
-}
