fa7cea66e261d22d52fe7e756ceb195582bf7293
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-28 20:57:23 +0500

    feat(ui): implement LoadingIndicatorAndroid

--- packages/ui/src/components/LoadingIndicator/exports.ts
+++ packages/ui/src/components/LoadingIndicator/exports.ts
@@ -1,4 +1,9 @@
 export {
+  LoadingIndicatorAndroid,
+  type LoadingIndicatorAndroidElementKey,
+  type LoadingIndicatorAndroidProps,
+} from './LoadingIndicatorAndroid/LoadingIndicatorAndroid.js';
+export {
   LoadingIndicatorIos,
   type LoadingIndicatorIosElementKey,
   type LoadingIndicatorIosProps,
