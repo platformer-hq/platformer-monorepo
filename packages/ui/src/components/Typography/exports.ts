b7238d1f254745757ba8657e569a5ab1710a9081
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-28 21:41:17 +0500

    feat(ui): export TypographyAndroid

--- packages/ui/src/components/Typography/exports.ts
+++ packages/ui/src/components/Typography/exports.ts
@@ -1,4 +1,10 @@
 export {
+  type TypographyAndroidVariant,
+  type TypographyAndroidProps,
+  type TypographyAndroidWeight,
+  TypographyAndroid,
+} from './TypographyAndroid/TypographyAndroid.jsx';
+export {
   type TypographyIosVariant,
   type TypographyIosProps,
   type TypographyIosWeight,
