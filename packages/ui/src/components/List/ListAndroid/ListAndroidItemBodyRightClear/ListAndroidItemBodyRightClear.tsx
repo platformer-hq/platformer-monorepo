9d7c2da1ce4929f7f149bb1e8940a12e1f28cc6b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-05-01 00:00:27 +0500

    fix(ui/ListAndroidItemBodyRightClear): use Xmark24 instead of XmarkFill28

--- packages/ui/src/components/List/ListAndroid/ListAndroidItemBodyRightClear/ListAndroidItemBodyRightClear.tsx
+++ packages/ui/src/components/List/ListAndroid/ListAndroidItemBodyRightClear/ListAndroidItemBodyRightClear.tsx
@@ -1,6 +1,6 @@
 import { cnCreate } from '@/css/cnCreate.js';
 
-import { XmarkFill28, type XmarkFill28Props } from '@/icons/common/28/XmarkFill28.jsx';
+import { Xmark24, type Xmark24Props } from '@/icons/common/24/Xmark24.jsx';
 import { omitClasses } from '@/helpers/omitClasses.js';
 import type { WithOptionalClasses } from '@/css/types.js';
 
@@ -10,10 +10,10 @@ import './ListAndroidItemBodyRightClear.scss';
 
 export type ListAndroidItemBodyRightClearElementKey = 'root';
 export type ListAndroidItemBodyRightClearProps =
-  & XmarkFill28Props
+  & Xmark24Props
   & WithOptionalClasses<ListAndroidItemBodyRightClearElementKey, ListAndroidItemBodyRightClearProps>;
 
 export function ListAndroidItemBodyRightClear(props: ListAndroidItemBodyRightClearProps) {
   const $cn = cnCreate(props, { root: e('item-body-right-clear') });
-  return <XmarkFill28 {...omitClasses(props)} size={20} class={$cn().root}/>;
+  return <Xmark24 {...omitClasses(props)} size={20} class={$cn().root}/>;
 }
