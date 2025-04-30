13dda8d952082dab8fb19bd1583b18b684dc4a96
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-30 22:15:59 +0500

    feat(ui): implement SwitchAndroid

--- packages/ui/src/components/Switch/exports.ts
+++ packages/ui/src/components/Switch/exports.ts
@@ -1,4 +1,9 @@
 export {
+  SwitchAndroid,
+  type SwitchAndroidProps,
+  type SwitchAndroidElementKey,
+} from './SwitchAndroid/SwitchAndroid.js';
+export {
   SwitchIos,
   type SwitchIosProps,
   type SwitchIosElementKey,
