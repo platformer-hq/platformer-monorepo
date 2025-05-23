9288dabeb13ff5f3c547b48a38b0e7314ee91ec0
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-05-23 20:51:24 +0500

    feat(swr): implement new key state KeyStateRevalidating. Enhance other states with the latest data

--- packages/swr/src/types/key-state.ts
+++ packages/swr/src/types/key-state.ts
@@ -1,20 +1,33 @@
+import type { CachedData } from './cache.js';
+
+export interface KeyLatestData<D> extends CachedData<D> {
+  state: 'fresh' | 'stale' | 'expired';
+}
+
 export interface KeyStatePending<D> {
   status: 'pending',
   data: Promise<D>;
+  latestData?: KeyLatestData<D>;
 }
 
-export interface KeyStateSuccess<D> {
+export interface KeyStateRevalidating<D> {
+  status: 'revalidating';
+  data: Promise<D>;
+  latestData?: KeyLatestData<D>;
+}
+
+export interface KeyStateSuccess<D> extends CachedData<D> {
   status: 'success';
-  data: D;
 }
 
 export interface KeyStateError<D, E> {
   status: 'error';
   error: E;
-  latestData?: D;
+  latestData?: KeyLatestData<D>;
 }
 
 export type KeyState<D, E = unknown> =
   | KeyStatePending<D>
+  | KeyStateRevalidating<D>
   | KeyStateSuccess<D>
   | KeyStateError<D, E>;
