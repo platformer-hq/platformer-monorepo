c854c1905798f6435eaffee030f7def5eb6d19b8
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-05-23 21:17:59 +0500

    chore(swr): actualize exports

--- packages/swr/src/index.ts
+++ packages/swr/src/index.ts
@@ -1,28 +1,3 @@
-export {
-  createSWRStore,
-  type CachedData,
-  type CreateSWRStoreOptions,
-  type KeyStateError,
-  type KeyState,
-  type KeyStateSuccess,
-  type KeyStatePending,
-  type DataCache,
-  type RevalidationCache,
-  type SWRStore,
-  type SWRStoreSubscribeFn,
-  type SWRStoreGetFn,
-  type SWRStoreMutateFnData,
-  type SWRStoreMutateFn,
-  type CreateSWRStoreKey,
-  type CreateSWRStoreFetcher,
-  type SWRStoreKeyValue,
-  type ObserversCache,
-} from './createSWRStore.js';
-export {
-  createSWRStoreSimple,
-  type SWRStoreSimple,
-  type SWRStoreSimpleGetFn,
-  type SWRStoreSimpleMutateFn,
-  type SWRStoreSimpleSubscribeFn,
-} from './createSWRSimpleStore.js';
-export type { ObservableListener, Observable } from './observable.js';
+export * from './createSWRStore.js';
+export * from './createSWRSimpleStore.js';
+export type * from './types/index.js';
