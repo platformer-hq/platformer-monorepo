30d1a7a77dd0212dfe34fe1064ca1aeeee85a3ec
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-05-24 15:17:57 +0500

    feat(shared): implement trigger method in useGqlMutationSuspenseless

--- packages/shared/src/gql/useGqlMutationSuspenseless.ts
+++ packages/shared/src/gql/useGqlMutationSuspenseless.ts
@@ -1,19 +1,41 @@
 import type { DocumentNode } from 'api';
-
+import type { UseSWRKeyStateWrapped, UseSWRSuspenselessResultUtils } from 'solid-swr';
 import { hapticFeedbackNotificationOccurred } from '@telegram-apps/sdk-solid';
 
-import { useGqlQuerySuspenseless, UseGqlQuerySuspenselessOptions } from './useGqlQuerySuspenseless.js';
+import {
+  useGqlQuerySuspenseless,
+  type UseGqlQuerySuspenselessOptions,
+  UseGqlQuerySuspenselessParams,
+} from './useGqlQuerySuspenseless.js';
+import type { UseGqlSuspenselessError } from 'solid-gql';
 
 export type UseGqlMutationSuspenselessOptions<D extends object, V extends object> =
   Omit<UseGqlQuerySuspenselessOptions<D, V>, 'freshAge' | 'staleAge'>;
 
+export interface UseGqlMutationSuspenselessTriggerFn<D, V> {
+  (variables: V): Promise<D>;
+}
+
+export interface UseGqlMutationSuspenselessResultUtils<D, V extends object>
+  extends Pick<UseSWRSuspenselessResultUtils<D, UseGqlQuerySuspenselessParams<V>>, 'mutate'> {
+  /**
+   * Triggers the mutation.
+   */
+  trigger: UseGqlMutationSuspenselessTriggerFn<D, V>;
+}
+
+export type UseGqlMutationSuspenselessResult<D, V extends object> = [
+  UseSWRKeyStateWrapped<D, UseGqlSuspenselessError>,
+  UseGqlMutationSuspenselessResultUtils<D, V>
+];
+
 export function useGqlMutationSuspenseless<D extends object, V extends object>(
   query: DocumentNode<D, V>,
   options?: UseGqlMutationSuspenselessOptions<D, V>,
-) {
+): UseGqlMutationSuspenselessResult<D, V> {
   options ||= {};
   const { onSuccess } = options;
-  return useGqlQuerySuspenseless<D, V>(query, undefined, {
+  const [keyState, { mutate }] = useGqlQuerySuspenseless<D, V>(query, undefined, {
     ...options,
     onSuccess(...args) {
       hapticFeedbackNotificationOccurred('success');
@@ -22,4 +44,11 @@ export function useGqlMutationSuspenseless<D extends object, V extends object>(
     staleAge: 0,
     freshAge: 0,
   });
+
+  return [keyState, {
+    mutate,
+    trigger(variables) {
+      return mutate(variables, false, true);
+    },
+  }];
 }
