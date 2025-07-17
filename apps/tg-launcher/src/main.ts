a6ab1fe107a7ea38a0b036b99312d186b8d833c6
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-07-17 19:39:42 +0500

    feat(tg-launcher): create initial files

---
+++ apps/tg-launcher/src/main.ts
@@ -0,0 +1,70 @@
+import './mockDevEnv.ts';
+
+import { createApp } from 'vue';
+import {
+  retrieveLaunchParams,
+  retrieveRawInitData,
+  retrieveRawLaunchParams,
+  createLogger,
+} from '@telegram-apps/sdk-vue';
+import { init as sentryInit } from '@sentry/vue';
+import { createI18n } from 'vue-i18n';
+
+import { init } from '@/init.ts';
+
+import Root from './components/Root/Root.vue';
+import type { Locale } from '@/types/common.ts';
+
+import './assets/index.scss';
+
+const launchParams = retrieveLaunchParams();
+const { tgWebAppPlatform: platform } = launchParams;
+const startParam = launchParams.tgWebAppStartParam || '';
+const debug = ['platformer_debug', 'p-debug'].some(v => startParam.includes(v)) || import.meta.env.DEV;
+const eruda = startParam.includes('p-eruda') || import.meta.env.DEV;
+
+try {
+  const { initialColors } = await init({
+    debug,
+    eruda,
+    mockForWebK: platform === 'web',
+    mockForMacOS: platform === 'macos',
+  });
+
+  const rawLaunchParams = retrieveRawLaunchParams();
+  const rawInitData = retrieveRawInitData();
+  const logger = createLogger('Platformer', {
+    textColor: 'white',
+    bgColor: 'chocolate',
+    shouldLog: debug,
+  });
+
+  let locale: Locale = 'en';
+  const { language_code: lang } = (retrieveLaunchParams().tgWebAppData || {}).user || {};
+  if (lang === 'ru' || lang === 'en') {
+    locale = lang;
+  }
+
+  const app = createApp(Root, {
+    debug,
+    platform,
+    rawLaunchParams,
+    rawInitData,
+    initialColors,
+    logger,
+    locale,
+  });
+  // TODO: make it async
+  sentryInit({
+    app,
+    dsn: 'https://8888815a88eb8e06bd1ac55195df9ab0@o992980.ingest.us.sentry.io/4508812774473728',
+    environment: import.meta.env.MODE,
+  });
+
+  app
+    .use(createI18n({ locale, legacy: false }))
+    .mount('#app');
+} catch (error) {
+  // TODO: Should properly handle the error.
+  console.error(error);
+}
---
+++ apps/tg-launcher/src/types/common.ts
@@ -0,0 +1,9 @@
+import type { RGB } from '@telegram-apps/sdk-vue';
+
+export type Locale = 'en' | 'ru';
+
+export type InitialColorsTuple = [
+  header: RGB | string,
+  background: RGB | undefined,
+  bottomBar: RGB | undefined
+];
