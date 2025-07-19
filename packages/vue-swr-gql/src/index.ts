38559763d107cd5bad4c1a506f060ef9ddf7d684
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-07-19 23:49:09 +0500

    feat(vue-swr-gql): add @graphql-typed-document-node/core dep and export it along with graphql-request and vue-swr

--- packages/vue-swr-gql/src/index.ts
+++ packages/vue-swr-gql/src/index.ts
@@ -1,3 +1,6 @@
+export type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+export * from 'graphql-request';
+export * from 'vue-swr';
 export * from './GraphQLError.js';
 export * from './provider.js';
 export * from './useGql.js';
