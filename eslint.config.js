3ec8f4119edd79173893ece8d07e0f297f59df18
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-07-20 01:03:29 +0500

    chore: reconfigure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,56 +4,47 @@ import eslint from '@eslint/js';
 import pluginVitest from '@vitest/eslint-plugin';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import pluginVue from 'eslint-plugin-vue';
-import tseslint from 'typescript-eslint';
 
-export default [
-  ...tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    {
-      languageOptions: {
-        parserOptions: {
-          project: [
-            './packages/*/tsconfig.json',
-            './apps/*/tsconfig.json'
-          ],
-          tsconfigRootDir: import.meta.dirname,
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-        },
-      },
-      rules: {
-        '@typescript-eslint/no-empty-object-type': 0,
-        '@typescript-eslint/no-explicit-any': 0,
-        '@typescript-eslint/no-unsafe-argument': 0,
-        '@typescript-eslint/no-unsafe-member-access': 0,
-        '@typescript-eslint/no-unsafe-assignment': 0,
-        '@typescript-eslint/no-unsafe-return': 0,
-        '@typescript-eslint/no-redundant-type-constituents': 0,
-        '@typescript-eslint/no-duplicate-type-constituents': 0,
-        '@typescript-eslint/no-unused-expressions': 0,
-        '@typescript-eslint/no-unsafe-call': 0,
-        '@typescript-eslint/no-namespace': 0,
-        '@typescript-eslint/no-misused-promises': 0,
-        'no-empty': 0,
+export default defineConfigWithVueTs(
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['**/__tests__/*'],
+  },
+  {
+    name: 'app/files-to-lint',
+    files: ['{packages,apps}/**/*.{js,ts,mts,tsx,vue}'],
+  },
+  eslint.configs.recommended,
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: [
+          './packages/*/tsconfig.json',
+          './apps/*/tsconfig.json'
+        ],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
-  ),
-  ...defineConfigWithVueTs(
-    {
-      name: 'app/files-to-lint',
-      files: ['apps/tg-launcher/**/*.{ts,mts,tsx,vue}'],
+    ignores: ['eslint.config.js'],
+    rules: {
+      'vue/multi-word-component-names': 0,
+      '@typescript-eslint/no-empty-object-type': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-unsafe-argument': 0,
+      '@typescript-eslint/no-unsafe-member-access': 0,
+      '@typescript-eslint/no-unsafe-assignment': 0,
+      '@typescript-eslint/no-unsafe-return': 0,
+      '@typescript-eslint/no-redundant-type-constituents': 0,
+      '@typescript-eslint/no-duplicate-type-constituents': 0,
+      '@typescript-eslint/no-unused-expressions': 0,
+      '@typescript-eslint/no-unsafe-call': 0,
+      '@typescript-eslint/no-namespace': 0,
+      '@typescript-eslint/no-misused-promises': 0,
+      'no-empty': 0,
     },
-    pluginVue.configs['flat/recommended'],
-    vueTsConfigs.recommended,
-    {
-      ...pluginVitest.configs.recommended,
-      files: ['apps/tg-launcher/src/**/__tests__/*'],
-    },
-    {
-      rules: {
-        'vue/multi-word-component-names': 0,
-      },
-    },
-  )
-];
+  },
+)
