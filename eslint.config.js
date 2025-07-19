9db7d6a2eac84cee8b8947e83f785c8caa8aef29
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-07-19 23:43:02 +0500

    chore: actualize eslint config. Remove solid configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,59 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import pluginVitest from '@vitest/eslint-plugin';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
 import tseslint from 'typescript-eslint';
-import solid from 'eslint-plugin-solid';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    files: ['./apps/{telegram-launcher,admin-panel}/src/**/*.{ts,tsx}'],
-    ...solid.configs.recommended,
-  },
-  {
-    languageOptions: {
-      parserOptions: {
-        project: [
-          './packages/*/tsconfig.json',
-          './apps/*/tsconfig.json'
-        ],
-        tsconfigRootDir: import.meta.dirname,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+export default [
+  ...tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          project: [
+            './packages/*/tsconfig.json',
+            './apps/*/tsconfig.json'
+          ],
+          tsconfigRootDir: import.meta.dirname,
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-empty-object-type': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-unsafe-argument': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
+        '@typescript-eslint/no-unsafe-assignment': 0,
+        '@typescript-eslint/no-unsafe-return': 0,
+        '@typescript-eslint/no-redundant-type-constituents': 0,
+        '@typescript-eslint/no-duplicate-type-constituents': 0,
+        '@typescript-eslint/no-unused-expressions': 0,
+        '@typescript-eslint/no-unsafe-call': 0,
+        '@typescript-eslint/no-namespace': 0,
+        '@typescript-eslint/no-misused-promises': 0,
+        'no-empty': 0,
       },
     },
-    rules: {
-      '@typescript-eslint/no-empty-object-type': 0,
-      '@typescript-eslint/no-explicit-any': 0,
-      '@typescript-eslint/no-unsafe-argument': 0,
-      '@typescript-eslint/no-unsafe-member-access': 0,
-      '@typescript-eslint/no-unsafe-assignment': 0,
-      '@typescript-eslint/no-unsafe-return': 0,
-      '@typescript-eslint/no-redundant-type-constituents': 0,
-      '@typescript-eslint/no-duplicate-type-constituents': 0,
-      '@typescript-eslint/no-unused-expressions': 0,
-      '@typescript-eslint/no-unsafe-call': 0,
-      '@typescript-eslint/no-namespace': 0,
-      '@typescript-eslint/no-misused-promises': 0,
-      'no-empty': 0,
+  ),
+  ...defineConfigWithVueTs(
+    {
+      name: 'app/files-to-lint',
+      files: ['apps/tg-launcher/**/*.{ts,mts,tsx,vue}'],
     },
-  },
-);
+    pluginVue.configs['flat/recommended'],
+    vueTsConfigs.recommended,
+    {
+      ...pluginVitest.configs.recommended,
+      files: ['apps/tg-launcher/src/**/__tests__/*'],
+    },
+    {
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
+    },
+  )
+];
