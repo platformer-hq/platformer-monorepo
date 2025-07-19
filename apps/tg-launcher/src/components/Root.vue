9bc5223dff2d10e4505c9a9e110b120379443ed3
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-07-20 01:46:29 +0500

    refactor(tg-launcher): refactor Root

--- apps/tg-launcher/src/components/Root.vue
+++ apps/tg-launcher/src/components/Root.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import type { Logger, Platform } from '@telegram-apps/sdk-vue';
-import { onErrorCaptured, ref } from 'vue';
 
-import type { InitialColorsTuple, Locale } from '@/types/common.ts';
-import { provideGlobals } from '@/providers/global.js';
-import ErrorStatusPage from '@/components/ErrorStatusPage.vue';
 import App, { type AppProps } from '@/components/App.vue';
+import ErrorBoundary from '@/components/ErrorBoundary.vue';
+import { provideGlobals } from '@/providers/global.js';
+import type { InitialColorsTuple, Locale } from '@/types/common.ts';
 
 interface RootProps extends AppProps {
   initialColors: InitialColorsTuple;
@@ -15,33 +14,16 @@ interface RootProps extends AppProps {
 }
 
 const { platform, locale, logger, initialColors } = defineProps<RootProps>();
+provideGlobals({ platform, locale, logger, initialColors });
 
-const error = ref<Error | undefined>();
-onErrorCaptured(err => {
-  error.value = err;
-});
-const resetError = () => {
-  error.value = undefined;
-};
-
-provideGlobals({
-  platform,
-  locale,
-  logger,
-  initialColors,
-});
-
+defineOptions({ inheritAttrs: false })
 </script>
 
 <template>
-  <ErrorStatusPage
-    v-if="error"
-    :error="{ type: 'unknown', cause: error }"
-    @retry="resetError"
-  />
-  <App
-    v-else
-    :raw-init-data="rawInitData"
-    :raw-launch-params="rawLaunchParams"
-  />
+  <ErrorBoundary>
+    <App
+      :raw-init-data="rawInitData"
+      :raw-launch-params="rawLaunchParams"
+    />
+  </ErrorBoundary>
 </template>
