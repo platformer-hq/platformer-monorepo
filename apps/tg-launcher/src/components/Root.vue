3f90892dd974c1bda8baaaad9faf770e8acf3eac
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-07-20 14:55:54 +0500

    refactor(tg-launcher): move App functionality to Root

--- apps/tg-launcher/src/components/Root.vue
+++ apps/tg-launcher/src/components/Root.vue
@@ -1,29 +1,54 @@
 <script setup lang="ts">
 import type { Logger, Platform } from '@telegram-apps/sdk-vue';
 
-import App, { type AppProps } from '@/components/App.vue';
-import ErrorBoundary from '@/components/ErrorBoundary.vue';
+import { extractLauncherOptions } from '@/helpers/extractLauncherOptions.js';
 import { provideGlobals } from '@/providers/global.js';
 import type { InitialColorsTuple, Locale } from '@/types/common.ts';
+import AppInitializer from './AppInitializer.vue';
+import ErrorBoundary from './ErrorBoundary.vue';
+import ErrorStatusPage from './ErrorStatusPage.vue';
 
-interface RootProps extends AppProps {
+const { platform, locale, logger, initialColors } = defineProps<{
   initialColors: InitialColorsTuple;
   locale: Locale;
   logger: Logger;
   platform: Platform;
-}
-
-const { platform, locale, logger, initialColors } = defineProps<RootProps>();
+  rawInitData?: string;
+  rawLaunchParams: string;
+}>();
 provideGlobals({ platform, locale, logger, initialColors });
 
-defineOptions({ inheritAttrs: false })
+defineOptions({ inheritAttrs: false });
+
+const opts = extractLauncherOptions();
 </script>
 
 <template>
-  <ErrorBoundary>
-    <App
-      :raw-init-data="rawInitData"
-      :raw-launch-params="rawLaunchParams"
-    />
-  </ErrorBoundary>
+  <main class="root">
+    <ErrorBoundary>
+      <ErrorStatusPage
+        v-if="!opts.ok"
+        :error="{ type: 'config-invalid', cause: opts.error }"
+      />
+      <ErrorStatusPage
+        v-else-if="!rawInitData"
+        :error="{ type: 'init-data-missing' }"
+      />
+      <AppInitializer
+        v-else
+        :app-id="opts.options.appId"
+        :fallback-url="opts.options.fallbackUrl"
+        :init-timeout="opts.options.initTimeout"
+        :load-timeout="opts.options.loadTimeout"
+        :raw-launch-params="rawLaunchParams"
+        :raw-init-data="rawInitData"
+      />
+    </ErrorBoundary>
+  </main>
 </template>
+
+<style>
+.root {
+  height: 100%;
+}
+</style>
