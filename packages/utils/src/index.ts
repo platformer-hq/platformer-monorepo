6526a1cc0ebaeab7b7e33154fb0076020413b27d
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-08-07 23:51:29 +0500

    chore(utils): remove the package

--- packages/utils/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './css/bem.js';
-export * from './css/cn.js';
-export * from './storage.js';
-export * from './casing.js';
-export * from './createLogger.js';
-export * from './lazy.js';
