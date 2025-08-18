398f1aa44201e7208f98141fc90b3ada43a81a06
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-08-18 23:22:36 +0500

    fix(adm/main): remove top-level await

--- apps/admin-panel/src/main.ts
+++ apps/admin-panel/src/main.ts
@@ -25,86 +25,86 @@ const { tgWebAppPlatform } = lp;
 const platform: 'ios' | 'android' = ['ios', 'macos'].includes(tgWebAppPlatform) ? 'ios' : 'android';
 // const platform = 'ios';
 
-try {
-  await init({
-    allowedParentOrigin: import.meta.env.PROD ? 'https://tgl.mini-apps.store' : '*',
-    debug: (lp.tgWebAppStartParam || '').includes('debug') || import.meta.env.DEV,
-    platform,
-  });
+init({
+  allowedParentOrigin: import.meta.env.PROD ? 'https://tgl.mini-apps.store' : '*',
+  debug: (lp.tgWebAppStartParam || '').includes('debug') || import.meta.env.DEV,
+  platform,
+})
+  .then(() => {
+    let locale: KnownLocale = 'en';
+    const { language_code: lang } = (lp.tgWebAppData || {}).user || {};
+    if (lang && isKnownLocale(lang)) {
+      locale = lang;
+    }
 
-  let locale: KnownLocale = 'en';
-  const { language_code: lang } = (lp.tgWebAppData || {}).user || {};
-  if (lang && isKnownLocale(lang)) {
-    locale = lang;
-  }
+    const app = createApp(Root, {
+      endpoint: import.meta.env.DEV ? '/gql' : 'https://mini-apps.store/api/gql',
+      platform,
+      // TODO: Verify init data existence.
+      initData: retrieveRawInitData()!,
+      appVersion: import.meta.env.APP_VERSION,
+    });
 
-  const app = createApp(Root, {
-    endpoint: import.meta.env.DEV ? '/gql' : 'https://mini-apps.store/gql',
-    platform,
-    // TODO: Verify init data existence.
-    initData: retrieveRawInitData()!,
-    appVersion: import.meta.env.APP_VERSION,
-  });
+    sentryInit({
+      app,
+      enabled: !!import.meta.env.VITE_SENTRY_ENABLED,
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      environment: import.meta.env.MODE,
+    });
 
-  sentryInit({
-    app,
-    enabled: !!import.meta.env.VITE_SENTRY_ENABLED,
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    environment: import.meta.env.MODE,
-  });
-
-  app
-    .use(VueQueryPlugin, {
-      queryClient: new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 600_000,
-            retry: 3,
-            retryDelay: failureCount => Math.pow(2, failureCount - 1) * 100,
-            refetchOnMount: 'always',
-            // @ts-expect-error Something is wrong with the lib types.
-            persister: experimental_createQueryPersister({
-              storage: sessionStorage,
-              maxAge: 60 * 60 * 1000,
-            }).persisterFn,
-          },
-          mutations: {
-            onError() {
-              hapticFeedbackNotificationOccurred('error');
+    app
+      .use(VueQueryPlugin, {
+        queryClient: new QueryClient({
+          defaultOptions: {
+            queries: {
+              staleTime: 600_000,
+              retry: 3,
+              retryDelay: failureCount => Math.pow(2, failureCount - 1) * 100,
+              refetchOnMount: 'always',
+              // @ts-expect-error Something is wrong with the lib types.
+              persister: experimental_createQueryPersister({
+                storage: sessionStorage,
+                maxAge: 60 * 60 * 1000,
+              }).persisterFn,
+            },
+            mutations: {
+              onError() {
+                hapticFeedbackNotificationOccurred('error');
+              },
             },
           },
-        },
-      }),
-    })
-    .use(createI18n({
-      locale,
-      legacy: false,
-      pluralRules: {
-        ru(count, choicesLength) {
-          const choiceFn = (count: number) => {
-            return () => {
-              if (Math.min(count, choicesLength) !== count) {
-                console.error(`unable to find i18n choice for count "${count}"`);
-              }
-              return count;
+        }),
+      })
+      .use(createI18n({
+        locale,
+        legacy: false,
+        pluralRules: {
+          ru(count, choicesLength) {
+            const choiceFn = (count: number) => {
+              return () => {
+                if (Math.min(count, choicesLength) !== count) {
+                  console.error(`unable to find i18n choice for count "${count}"`);
+                }
+                return count;
+              };
             };
-          };
-          const zero = choiceFn(0);
-          if (count === 0) {
-            return zero();
-          }
-          const one = choiceFn(1);
-          const two = choiceFn(2);
-          const many = choiceFn(3);
-          return (count % 100 > 4 && count % 100 < 20)
-            ? many()
-            : [many, one, two, two, two, many][Math.min(count % 10, 5)]();
+            const zero = choiceFn(0);
+            if (count === 0) {
+              return zero();
+            }
+            const one = choiceFn(1);
+            const two = choiceFn(2);
+            const many = choiceFn(3);
+            return (count % 100 > 4 && count % 100 < 20)
+              ? many()
+              : [many, one, two, two, two, many][Math.min(count % 10, 5)]();
+          },
         },
-      },
-    }))
-    .use(createAppRouter())
-    .mount('#app');
-} catch (error) {
-  // TODO: Should properly handle the error.
-  console.error(error);
-}
+      }))
+      .use(createAppRouter())
+      .mount('#app');
+  })
+  .catch(error => {
+    // TODO: Should properly handle the error.
+    console.error(error);
+  });
