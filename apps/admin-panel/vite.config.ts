2b0b36ce95d100fcfb7fb7b9bb457abf54ed5933
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-08-19 01:36:21 +0500

    build(adm): remove base option

--- apps/admin-panel/vite.config.ts
+++ apps/admin-panel/vite.config.ts
@@ -11,54 +11,51 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 import packageJson from './package.json' with { type: 'json' };
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'serve' ? '/' : '/latest',
-    css: {
-      postcss: { plugins: [autoprefixer(), cssnano()] },
-    },
-    plugins: [
-      vue({
-        features: {
-          // TODO: This one breaks hot reload.
-          // componentIdGenerator(id, source, isProduction, getHash) {
-          //   return basename(id).split('.')[0] + `-${getHash(id + source)}`;
-          // },
-        },
-      }),
-      vueDevTools(),
-      process.env.ANALYZER ? analyzer() : undefined,
-      process.env.HTTPS ? mkcert() : undefined,
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig({
+  css: {
+    postcss: { plugins: [autoprefixer(), cssnano()] },
+  },
+  plugins: [
+    vue({
+      features: {
+        // TODO: This one breaks hot reload.
+        // componentIdGenerator(id, source, isProduction, getHash) {
+        //   return basename(id).split('.')[0] + `-${getHash(id + source)}`;
+        // },
       },
+    }),
+    vueDevTools(),
+    process.env.ANALYZER ? analyzer() : undefined,
+    process.env.HTTPS ? mkcert() : undefined,
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    build: {
-      target: browserslistToEsbuild(),
-      outDir: 'dist',
-      emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            mixpanel: ['mixpanel-browser'],
-            sentry: ['@sentry/vue'],
-            vue: ['vue'],
-          },
+  },
+  build: {
+    target: browserslistToEsbuild(),
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mixpanel: ['mixpanel-browser'],
+          sentry: ['@sentry/vue'],
+          vue: ['vue'],
         },
       },
     },
-    server: {
-      proxy: {
-        '/api': 'https://mini-apps.store/',
-        // '/api': 'http://localhost:10000',
-      },
-      // Exposes your dev server and makes it accessible for the devices in the same network.
-      host: true,
+  },
+  server: {
+    proxy: {
+      '/api': 'https://mini-apps.store/',
+      // '/api': 'http://localhost:10000',
     },
-    define: {
-      'import.meta.env.APP_VERSION': JSON.stringify(packageJson.version),
-    },
-  };
+    // Exposes your dev server and makes it accessible for the devices in the same network.
+    host: true,
+  },
+  define: {
+    'import.meta.env.APP_VERSION': JSON.stringify(packageJson.version),
+  },
 });
