6abcc6a76b230d1cab20a75cf546ea7141a92b1b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-08-22 01:34:07 +0500

    feat(adm/AppTestGroupsView): implement TextWithLink

---
+++ apps/admin-panel/src/views/AppTestGroupsView/TextWithLink.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  html: string;
+}>();
+const emit = defineEmits<{
+  linkClick: [];
+}>();
+
+const onClick = (e: MouseEvent) => {
+  e.target instanceof HTMLAnchorElement && emit('linkClick');
+};
+</script>
+
+<template>
+  <span
+    class="root"
+    @click="onClick"
+    v-html="html"
+  />
+</template>
+
+<style scoped lang="scss">
+@use "@/vue-ui/styles/mixins";
+
+.root {
+  :deep(a) {
+    color: var(--theme-accent-text-color);
+    @include mixins.clickable;
+  }
+}
+</style>
