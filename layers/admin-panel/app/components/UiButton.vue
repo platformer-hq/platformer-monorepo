f4b33588951863aa5043fc1fe00ccabdfc1dd513
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-01 02:41:22 +0500

    feat(components): implement ButtonBase and UiButton

---
+++ layers/admin-panel/app/components/UiButton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { ButtonBaseProps } from './ButtonBase.vue';
+
+interface Props extends ButtonBaseProps {
+  /**
+   * @default 'regular'
+   */
+  variant?: 'regular' | 'small' | 'multiline';
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'regular',
+  ripples: undefined,
+  highlightOnActive: undefined,
+  pressable: undefined,
+  clickable: undefined,
+});
+const { b } = bem('ui-button');
+</script>
+
+<template>
+  <ButtonBase v-bind="$props" :as :clickable :class="b(variant)">
+    <slot/>
+  </ButtonBase>
+</template>
+
+<style lang="scss">
+.ui-button {
+  &--small {
+    padding: 4px 12px;
+    min-height: 28px;
+    border-radius: 1000px;
+  }
+
+  &--multiline {
+    padding: 8px 24px;
+    min-height: 62px;
+    border-radius: 16px;
+  }
+
+  &--regular {
+    padding: 10px 24px;
+    min-height: 50px;
+    border-radius: 25px;
+  }
+}
+</style>
