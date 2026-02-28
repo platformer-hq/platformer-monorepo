f94c48a0e12340062f2e04c406aad68c2eb3c65a
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-02-28 21:40:44 +0500

    feat(gqlRequest): set a specific error type

--- layers/graphql/app/utils/gqlRequest.ts
+++ layers/graphql/app/utils/gqlRequest.ts
@@ -1,6 +1,6 @@
 import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { taskEither } from 'fp-ts';
-import type { GraphQLClient, RequestExtendedOptions, Variables } from 'graphql-request';
+import type { ClientError, GraphQLClient, RequestExtendedOptions, Variables } from 'graphql-request';
 
 /**
  * Performs a GraphQL request using specified client.
@@ -10,12 +10,12 @@ export function gqlRequest<T, V extends Variables>({ client, document, variables
   client: GraphQLClient;
   document: TypedDocumentNode<T, V>;
   variables: V;
-}): taskEither.TaskEither<Error, T> {
+}): taskEither.TaskEither<ClientError, T> {
   // TODO: Add "retry" option.
   return taskEither.tryCatch(() => {
     return client.request({
       document,
       variables,
     } as unknown as RequestExtendedOptions<V, T>);
-  }, e => e as Error);
+  }, e => e as ClientError);
 }
