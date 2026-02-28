c2165c55f226bb3cbe68ab5152a160c35a645f2d
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-01 01:04:46 +0500

    feat(components): add VerticalPaddings

---
+++ layers/admin-panel/app/components/VerticalPaddings.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { KnownHtmlTag } from '#layers/ui-kit';
+
+const { as = 'div' } = defineProps<{
+  /**
+   * @default 'div'
+   */
+  as?: KnownHtmlTag;
+}>();
+</script>
+
+<template>
+  <component :is="as" class="vertical-paddings">
+    <slot/>
+  </component>
+</template>
+
+<style>
+.vertical-paddings {
+  padding-block: 16px;
+}
+</style>
