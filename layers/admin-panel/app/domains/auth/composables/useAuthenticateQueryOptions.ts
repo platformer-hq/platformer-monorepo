1fd6dfd6e1e132bbb0999aacfe9594983b858108
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-02-28 21:49:53 +0500

    feat: implement useAuthenticateQueryOptions

---
+++ layers/admin-panel/app/domains/auth/composables/useAuthenticateQueryOptions.ts
@@ -0,0 +1,21 @@
+import { queryOptions } from '@tanstack/vue-query';
+import { function as fn, taskEither } from 'fp-ts';
+
+import { AuthenticateDocument } from './_api/operations';
+
+export function useAuthenticateQueryOptions() {
+  const request = useMakeGqlApiRequest();
+  const { initDataRaw } = useTmaStore();
+  return queryOptions({
+    queryKey: [AuthenticateDocument],
+    queryFn: throwify(() => {
+      return fn.pipe(
+        request({ document: AuthenticateDocument, variables: { initData: initDataRaw } }),
+        taskEither.map(({ authenticateTelegram }) => ({
+          token: authenticateTelegram.token,
+          expiresAt: new Date(authenticateTelegram.expiresAt),
+        })),
+      );
+    }),
+  });
+}
