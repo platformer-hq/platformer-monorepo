5be2eeca08681b074e9f93a762db04a6042fe664
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-02-28 19:44:12 +0500

    fix(AutoSidePaddings): replace with SidePaddings

---
+++ layers/admin-panel/app/components/SidePaddings.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { KnownHtmlTag } from '#layers/ui-kit';
+
+const { as = 'div' } = defineProps<{
+  /**
+   * @default 'div'
+   */
+  as?: KnownHtmlTag;
+}>();
+</script>
+
+<template>
+  <component :is="as" class="side-paddings">
+    <slot/>
+  </component>
+</template>
+
+<style>
+.side-paddings {
+  padding: 0 16px;
+}
+</style>
