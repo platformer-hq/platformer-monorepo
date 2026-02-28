f099c5577390844eb28af4bdcf2bbfbbe7beabde
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-02-28 21:48:50 +0500

    feat(pages): implement home page

--- layers/admin-panel/app/pages-components/HomePage.vue
+++ layers/admin-panel/app/pages-components/HomePage.vue
@@ -1,26 +1,149 @@
 <script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
 
+import {
+  IconApps30,
+  IconLockFill28,
+  IconPersonHanshakeFill28,
+  IconTrades30,
+  IconUserCircleFill28,
+} from '#components';
+
+import { usePageDataQueryOptions } from './_api/usePageDataQueryOptions';
+
+const { data, isPending } = useQuery(usePageDataQueryOptions());
+const { t } = useI18n({
+  messages: {
+    en: {
+      apps: 'Applications',
+      invites: 'Management Invites',
+      transferRequests: 'App Transfer Requests',
+      account: 'Аккаунт',
+      privacy: 'Privacy and Security',
+    },
+    ru: {
+      apps: 'Приложения',
+      invites: 'Приглашения на управление',
+      transferRequests: 'Трансферы приложений',
+      account: 'Account',
+      privacy: 'Приватность и безопасность',
+    },
+  },
+});
+const platform = useTmaPlatform();
+
+const createIconComponent = <C extends Component>(component: C) => ({
+  kind: 'component' as const,
+  component,
+});
+const createCustomIconComponent = <C extends Component>(
+  component: C,
+  size: number,
+  bgColor: ColorReferenceAnyColor,
+) => ({
+  kind: 'custom' as const,
+  component,
+  size,
+  bgColor,
+});
+const sections = computed(() => {
+  return [
+    [{
+      title: t('apps'),
+      name: PAGE_NAME_APPS,
+      icon: createIconComponent(IconApps30),
+    }, {
+      title: t('invites'),
+      name: PAGE_NAME_MANAGEMENT_INVITES,
+      icon: createCustomIconComponent(IconPersonHanshakeFill28, 20, '#FF9500'),
+      count: data.value?.invitesCount,
+      hasCounter: true,
+    }, {
+      title: t('transferRequests'),
+      name: PAGE_NAME_TRANSFER_REQUESTS,
+      icon: createIconComponent(IconTrades30),
+      count: data.value?.transferRequestsCount,
+      hasCounter: true,
+    }],
+    [{
+      title: t('account'),
+      name: PAGE_NAME_ACCOUNT,
+      icon: createCustomIconComponent(IconUserCircleFill28, 24, '#FF3B30'),
+    }, {
+      title: t('privacy'),
+      name: PAGE_NAME_PRIVACY,
+      icon: createCustomIconComponent(IconLockFill28, 24, '#8E8E93'),
+    }],
+  ];
+});
+
+onMounted(() => {
+  preloadRouteComponents({ name: PAGE_NAME_APPS });
+  preloadRouteComponents({ name: PAGE_NAME_MANAGEMENT_INVITES });
+  preloadRouteComponents({ name: PAGE_NAME_TRANSFER_REQUESTS });
+  preloadRouteComponents({ name: PAGE_NAME_ACCOUNT });
+  preloadRouteComponents({ name: PAGE_NAME_PRIVACY });
+});
 </script>
 
 <template>
-  <ViewBase :back="false">
-    <ViewContent>
-      <ul>
-        <li>
-          <NuxtLink :to="{name: PAGE_NAME_APPS}">
-            Apps
-          </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink :to="{name: PAGE_NAME_SETTINGS}">
-            Settings
-          </NuxtLink>
-        </li>
-      </ul>
-    </ViewContent>
-  </ViewBase>
+  <PageBase :back="false">
+    <PageContent>
+      <SidePaddings>
+        <AutoSection
+          v-for="(section, sectionIdx) in sections"
+          :key="sectionIdx"
+          :style="sectionIdx > 0 ? {marginTop: '16px'} : undefined"
+          list-bg-color="secondary-bg"
+        >
+          <AutoList>
+            <AutoListItem
+              v-for="item in section"
+              :key="item.name"
+              clickable
+              @click="navigateTo({name: item.name})"
+            >
+              <template #left>
+                <AutoListItemLeft>
+                  <AutoListItemLeftIcon square>
+                    <AutoListItemLeftIconElement
+                      square
+                      :style="item.icon.kind === 'custom' ? {
+                        background: colorReference(item.icon.bgColor) || undefined,
+                        color: 'white',
+                      } : undefined"
+                    >
+                      <component
+                        :is="item.icon.component"
+                        :size="item.icon.kind === 'custom' ? item.icon.size : undefined"
+                      />
+                    </AutoListItemLeftIconElement>
+                  </AutoListItemLeftIcon>
+                </AutoListItemLeft>
+              </template>
+              <template #bodyLeftLabel>
+                <AutoListItemBodyLeftLabel>
+                  {{ item.title }}
+                </AutoListItemBodyLeftLabel>
+              </template>
+              <template #bodyRight>
+                <AutoListItemBodyRight>
+                  <ShimmerBox
+                    v-if="'hasCounter' in item && item.count === undefined && isPending"
+                    :width="24"
+                    :height="24"
+                    rounded
+                  />
+                  <AutoListItemBodyRightBadge v-else-if="'count' in item && item.count">
+                    {{ item.count }}
+                  </AutoListItemBodyRightBadge>
+                  <AutoListItemBodyRightChevron v-if="platform.isMappedIos"/>
+                </AutoListItemBodyRight>
+              </template>
+            </AutoListItem>
+          </AutoList>
+        </AutoSection>
+      </SidePaddings>
+    </PageContent>
+  </PageBase>
 </template>
-
-<style scoped>
-
-</style>
---
+++ layers/admin-panel/app/pages-components/_api/usePageDataQueryOptions.ts
@@ -0,0 +1,27 @@
+import { queryOptions } from '@tanstack/vue-query';
+import { function as fn, taskEither } from 'fp-ts';
+
+import { HomePageDataDocument } from './operations';
+
+type Err = ApiGraphQLError;
+interface Result {
+  transferRequestsCount: number;
+  invitesCount: number;
+}
+
+export function usePageDataQueryOptions() {
+  const request = useMakeGqlApiRequest();
+  const queryKey = [HomePageDataDocument] as const;
+  return queryOptions<Result, Err, Result, typeof queryKey>({
+    queryKey,
+    queryFn: throwify(() => {
+      return fn.pipe(
+        request({ document: HomePageDataDocument, variables: {} }),
+        taskEither.map(({ currentUser }) => ({
+          transferRequestsCount: currentUser.appTransferRequests.length,
+          invitesCount: currentUser.managementInvites.length,
+        })),
+      );
+    }),
+  });
+}
