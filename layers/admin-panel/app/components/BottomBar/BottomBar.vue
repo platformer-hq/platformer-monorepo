b7fc63778d09fea42af24a5c99737177c3b6700c
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-01 02:41:01 +0500

    feat(components): implement BottomBar

---
+++ layers/admin-panel/app/components/BottomBar/BottomBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { b, e } = bem('bottom-bar');
+</script>
+
+<template>
+  <SafeAreaInsets :class="b()" left right bottom>
+    <div :class="e('inner')">
+      <slot/>
+    </div>
+  </SafeAreaInsets>
+</template>
+
+<style lang="scss">
+.bottom-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  &__inner {
+    padding: 0 16px 16px;
+  }
+}
+</style>
---
+++ layers/admin-panel/app/components/BottomBar/BottomBarEnterUpTransition.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const transition = createReversibleTransition({
+  animatedProperties: {
+    transform: ['translateY(100%)', 'translateY(0)'],
+    opacity: [0, 0, 1],
+  },
+  animationOptions: {
+    duration: 500,
+    easing: 'ease-in-out',
+  },
+});
+</script>
+
+<template>
+  <Transition v-bind="transition" :css="false">
+    <slot/>
+  </Transition>
+</template>
