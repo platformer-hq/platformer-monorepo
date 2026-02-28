ce14218df42cba9d4e432ab46b4152cffd862a8c
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-01 00:47:55 +0500

    feat(app): authenticate in the app root

--- layers/admin-panel/app/app.vue
+++ layers/admin-panel/app/app.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
 import { miniApp } from '@tma.js/sdk-vue';
+
+const apiStore = useApiStore();
+
+if (import.meta.client) {
+  const { data: response } = useQuery({
+    ...useAuthenticateQueryOptions(),
+    staleTime: 0,
+    retry(_, error) {
+      return !ApiGraphQLError.is(error) || !error.isOfType('ERR_INIT_DATA_INVALID');
+    },
+  });
+
+  watch(response, data => {
+    if (data) {
+      apiStore.setToken({ token: data.token, expiresAt: data.expiresAt });
+    }
+  }, { immediate: true });
+
+  if (!isPageReload()) {
+    await navigateTo({ name: PAGE_NAME_MAIN, replace: true });
+  }
+}
 </script>
 
 <template>
   <ClientOnly>
-    <NavigationStateProvider>
+    <NavigationStateProvider v-if="apiStore.token">
       <LayoutBody v-slot="{transition}" @ready="miniApp.ready">
         <NuxtPage :page-key="route => route.path" :transition="transition"/>
       </LayoutBody>
