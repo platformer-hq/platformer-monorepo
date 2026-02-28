f114af7ed1522a621047b52581b0a96689d708e5
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-02-28 19:57:54 +0500

    feat(ui-kit): implement Section*

---
+++ layers/ui-kit/app/components/Section/SectionIos.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineSlots<{
+  header(): unknown;
+  default(): unknown;
+  footer(): unknown;
+}>();
+</script>
+
+<template>
+  <section class="section-ios">
+    <slot name="header"/>
+    <div class="section-ios__list">
+      <slot/>
+    </div>
+    <slot name="footer"/>
+  </section>
+</template>
+
+<style lang="scss">
+.section-ios {
+  &__list {
+    border-radius: 26px;
+    overflow: hidden;
+    background: var(--section-ios-list-bg);
+  }
+}
+</style>
