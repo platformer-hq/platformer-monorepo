35222356693c7560df63d673086b493f6024bdf1
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-03 00:21:22 +0500

    feat(ListAndroidItem): switch to new ripples. Add clickable modifier. Reduce structure

--- layers/ui-kit/app/components/List/ListAndroid/ListAndroidItem.vue
+++ layers/ui-kit/app/components/List/ListAndroid/ListAndroidItem.vue
@@ -3,7 +3,7 @@ import { provideListItemOptions } from './provider.js';
 
 export type ListAndroidItemVariant = 'regular' | 'accent' | 'destructive' | 'placeholder';
 
-const { variant = 'regular', large = false } = defineProps<{
+const props = withDefaults(defineProps<{
   /**
    * True if the element is clickable. This will add some additional visual changes to the element.
    */
@@ -17,7 +17,9 @@ const { variant = 'regular', large = false } = defineProps<{
    * @default 'regular'
    */
   variant?: ListAndroidItemVariant;
-}>();
+}>(), {
+  variant: 'regular',
+});
 defineSlots<{
   left(): unknown;
   body(): unknown;
@@ -28,64 +30,69 @@ defineSlots<{
   bodyRight(): unknown;
 }>();
 
-const { b, e } = bem('list-android-item');
+const { b } = bem('list-android-item');
 
 provideListItemOptions({
-  large: computed(() => large),
+  large: computed(() => props.large ?? false),
 });
 
 const bodyLeftSlots = [
   { id: 'bodyLeftLabel', name: 'label' },
   { id: 'bodyLeftSubtitle', name: 'subtitle' },
 ] as const;
+
+const rootRef = useTemplateRef('root');
+useRipples({
+  enabled: () => props.clickable,
+  containerRef: rootRef,
+  clickRef: rootRef,
+});
 </script>
 
 <template>
-  <li :class="b(variant)">
-    <UiRipples as="div" :enabled="clickable" :class="e('inner')">
-      <slot name="left"/>
-      <slot name="body">
-        <ListAndroidItemBody>
-          <template v-if="$slots.bodyInput" #input>
-            <slot name="bodyInput" />
-          </template>
-          <template v-else #left>
-            <slot name="bodyLeft">
-              <ListAndroidItemBodyLeft v-if="bodyLeftSlots.some(s => s.id in $slots)">
-                <template
-                  v-for="{id, name} in bodyLeftSlots.filter(s => s.id in $slots)"
-                  :key="id"
-                  #[name]
-                >
-                  <slot :name="id" />
-                </template>
-              </ListAndroidItemBodyLeft>
-            </slot>
-          </template>
-          <template #right>
-            <slot name="bodyRight"/>
-          </template>
-        </ListAndroidItemBody>
-      </slot>
-    </UiRipples>
+  <li ref="root" :class="b(variant, {clickable})">
+    <slot name="left"/>
+    <slot name="body">
+      <ListAndroidItemBody>
+        <template v-if="$slots.bodyInput" #input>
+          <slot name="bodyInput" />
+        </template>
+        <template v-else #left>
+          <slot name="bodyLeft">
+            <ListAndroidItemBodyLeft v-if="bodyLeftSlots.some(s => s.id in $slots)">
+              <template
+                v-for="{id, name} in bodyLeftSlots.filter(s => s.id in $slots)"
+                :key="id"
+                #[name]
+              >
+                <slot :name="id" />
+              </template>
+            </ListAndroidItemBodyLeft>
+          </slot>
+        </template>
+        <template #right>
+          <slot name="bodyRight"/>
+        </template>
+      </ListAndroidItemBody>
+    </slot>
   </li>
 </template>
 
 <style lang="scss">
+@use "@/domains/styles/mixins";
+
 .list-android-item {
   display: block;
+  box-sizing: border-box;
+  display: flex;
+  padding: 0 0 0 20px;
+  background: transparent;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
 
-  &__inner {
-    appearance: none;
-    border: none;
-    box-sizing: border-box;
-    width: 100%;
-    display: flex;
-    padding: 0 0 0 20px;
-    background: transparent;
-    text-align: left;
-    color: inherit;
-    height: 100%;
+  &--clickable {
+    @include mixins.clickable;
   }
 
   @each $variant in ("regular", "destructive", "accent", "placeholder") {
