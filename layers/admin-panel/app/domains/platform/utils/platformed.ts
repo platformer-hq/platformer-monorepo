ee4366db6ddc28118568ec087f6f51e403c635fd
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-03 01:21:41 +0500

    fix(platformed): rework the util

--- layers/admin-panel/app/domains/platform/utils/platformed.ts
+++ layers/admin-panel/app/domains/platform/utils/platformed.ts
@@ -1,4 +1,6 @@
-import { h, type Component } from 'vue';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { defineComponent, h, ref, type Component } from 'vue';
+import type { ComponentExposed } from 'vue-component-type-helpers';
 
 import type { KnownPlatform } from '../types.js';
 
@@ -7,13 +9,45 @@ interface Options<C> extends Partial<Record<KnownPlatform, C>> {
 }
 
 export function platformed<C extends Component>({ common, ios, android }: Options<C>): C {
-  // @ts-expect-error TODO: research the problem
-  return ((props, options) => {
-    const store = useTmaStore();
-    return h(
-      ({ android, ios })[store.platform.mapped] || common,
-      props,
-      options.slots,
-    );
-  });
+  return defineComponent({
+    name: 'Platformed',
+    setup(props, { attrs, slots, expose }) {
+      const platform = useTmaPlatform();
+      const innerRef = ref<ComponentExposed<C>>();
+
+      expose(
+        new Proxy({}, {
+          get(_, prop) {
+            const instance = innerRef.value;
+            const v = instance?.[prop];
+            return typeof v === 'function'
+              ? (...args: any[]) => v.apply(instance, args)
+              : v;
+          },
+          set(_, prop, value) {
+            if (!innerRef.value) {
+              return false;
+            }
+            innerRef.value[prop] = value;
+            return true;
+          },
+          has(_, prop) {
+            return innerRef.value ? prop in innerRef.value : false;
+          },
+        }),
+      );
+
+      return () => {
+        return h(
+          ({ android, ios })[platform.value.mapped] || common,
+          {
+            ref: innerRef,
+            ...(attrs as any),
+            ...(props as any),
+          },
+          slots,
+        );
+      };
+    },
+  }) as unknown as C;
 }
