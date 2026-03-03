b654b7209d6cbffeb4907a81ec911e90c3bc5c4b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-03 23:50:08 +0500

    chore: minor changes

--- layers/admin-panel/app/pages-components/_api/operations.ts
+++ layers/admin-panel/app/pages-components/_api/operations.ts
@@ -5,7 +5,7 @@ import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-
 export type HomePageDataQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
-export type HomePageDataQuery = { __typename?: 'Query', currentUser: { __typename?: 'CurrentUser', id: string, managementInvites: Array<{ __typename: 'AppManagementInvite' }>, appTransferRequests: Array<{ __typename: 'AppTransferRequest' }> } };
+export type HomePageDataQuery = { __typename?: 'Query', currentUser: { __typename?: 'CurrentUser', id: number, managementInvites: Array<{ __typename: 'AppManagementInvite' }>, appTransferRequests: Array<{ __typename: 'AppTransferRequest' }> } };
 
 
 export const HomePageDataDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"HomePageData"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"currentUser"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"managementInvites"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}},{"kind":"Field","name":{"kind":"Name","value":"appTransferRequests"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"__typename"}}]}}]}}]}}]} as unknown as DocumentNode<HomePageDataQuery, HomePageDataQueryVariables>;
--- layers/admin-panel/app/pages-components/apps/create/operations.ts
+++ layers/admin-panel/app/pages-components/apps/create/operations.ts
@@ -7,7 +7,7 @@ export type CreateAppMutationVariables = Types.Exact<{
 }>;
 
 
-export type CreateAppMutation = { __typename?: 'Mutation', createApp: { __typename?: 'App', id: string, title: string, privacy: Types.AppPrivacy, currentUserRole: Types.AppRole } };
+export type CreateAppMutation = { __typename?: 'Mutation', createApp: { __typename?: 'App', id: number, title: string, privacy: Types.AppPrivacy, currentUserRole: Types.AppRole } };
 
 
 export const CreateAppDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateApp"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"title"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createApp"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"title"},"value":{"kind":"Variable","name":{"kind":"Name","value":"title"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"privacy"}},{"kind":"Field","name":{"kind":"Name","value":"currentUserRole"}}]}}]}}]} as unknown as DocumentNode<CreateAppMutation, CreateAppMutationVariables>;
