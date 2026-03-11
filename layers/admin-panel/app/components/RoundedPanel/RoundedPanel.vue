344b2ccb1c8f1d90622710698ef41828392e40a8
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-11 14:01:26 +0500

    feat(components): implement RoundedPanel

---
+++ layers/admin-panel/app/components/RoundedPanel/RoundedPanel.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { b } = bem('rounded-panel');
+const platform = useTmaPlatform();
+</script>
+
+<template>
+  <div :class="b(platform.mapped)">
+    <slot/>
+  </div>
+</template>
+
+<style lang="scss">
+.rounded-panel {
+  &--ios {
+    border-radius: 26px;
+  }
+  &--android {
+    border-radius: 16px;
+  }
+}
+</style>
