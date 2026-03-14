0b1fff392461c56f395b6132f60b4fa7c16cdaaf
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-14 14:51:06 +0500

    feat(ui-kit): actualize Android transition. Refactor other transitions code

--- layers/ui-kit/app/page-transitions/utils/getAndroidPageTransitionOptions.ts
+++ layers/ui-kit/app/page-transitions/utils/getAndroidPageTransitionOptions.ts
@@ -1,28 +1,30 @@
 import './android-page-transition.scss';
 
+import { createReversibleTransition } from '@ui-kit/transitions/utils/transitions';
+
 export function getAndroidPageTransitionOptions(): Required<Pick<
   UsePageTransitionOptions,
   'afterEnter' | 'animate' | 'beforeAnimate'
 >> {
   const { b } = bem('android-page-transition');
+  const duration = 200;
+  const tr = createReversibleTransition({
+    animatedProperties: {
+      transform: ['translateX(10%)', 'translateX(0)'],
+      opacity: [0, 1],
+    },
+    animationOptions: { duration, easing: 'ease-out' },
+  });
+
   return {
     beforeAnimate({ el, page, transition }) {
       el.classList.add(...b(page, transition).split(' '));
     },
     animate({ transition, page, el, done }) {
       if (page === 'left') {
-        return setTimeout(done, 300);
+        return setTimeout(done, duration);
       }
-      const transform = ['translateX(100%)', 'translateX(0)'];
-      el
-        .animate({ transform: transition === 'leave' ? transform.reverse() : transform }, {
-          duration: 300,
-          easing: 'ease-out',
-        })
-        .finished
-        .then(() => {
-          done();
-        });
+      tr[transition === 'enter' ? 'onEnter' : 'onLeave'](el, done);
     },
     afterEnter({ el, page }) {
       el.classList.remove(...b(page, 'enter').split(' '));
--- layers/ui-kit/app/page-transitions/utils/getIosPageTransitionOptions.ts
+++ layers/ui-kit/app/page-transitions/utils/getIosPageTransitionOptions.ts
@@ -1,29 +1,30 @@
 import './ios-page-transition.scss';
 
+import { createReversibleTransition } from '@ui-kit/transitions/utils/transitions';
+
 export function getIosPageTransitionOptions(): Required<Pick<
   UsePageTransitionOptions,
   'afterEnter' | 'animate' | 'beforeAnimate'
 >> {
   const { b } = bem('ios-page-transition');
+  const animationOptions = { duration: 300, easing: 'ease-out' };
+  const leftTransition = createReversibleTransition({
+    animatedProperties: { transform: ['translateX(-100px)', 'translateX(0)'] },
+    animationOptions,
+  });
+  const rightTransition = createReversibleTransition({
+    animatedProperties: { transform: ['translateX(100%)', 'translateX(0)'] },
+    animationOptions,
+  });
+
   return {
     beforeAnimate({ el, page, transition }) {
       el.classList.add(...b(page, transition).split(' '));
     },
     animate({ transition, page, el, done }) {
-      const leftTransform = ['translateX(-100px)', 'translateX(0)'];
-      const rightTransform = ['translateX(100%)', 'translateX(0)'];
-      if (transition === 'leave') {
-        [leftTransform, rightTransform].forEach(arr => arr.reverse());
-      }
-      el
-        .animate({ transform: page === 'right' ? rightTransform : leftTransform }, {
-          duration: 300,
-          easing: 'ease-in-out',
-        })
-        .finished
-        .then(() => {
-          done();
-        });
+      (page === 'left' ? leftTransition : rightTransition)[
+        transition === 'enter' ? 'onEnter' : 'onLeave'
+      ](el, done);
     },
     afterEnter({ el, page }) {
       el.classList.remove(...b(page, 'enter').split(' '));
