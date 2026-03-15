adc41d0f02fd008fa5ade4b2ce5ea94e13284e49
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-15 22:09:56 +0500

    feat(useMakeGqlApiRequest): export GqlApiRequestFn

--- layers/admin-panel/app/domains/api/composables/useMakeGqlApiRequest.ts
+++ layers/admin-panel/app/domains/api/composables/useMakeGqlApiRequest.ts
@@ -2,13 +2,15 @@ import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
 import * as fp from 'fp-ts';
 import type { Variables } from 'graphql-request';
 
-export function useMakeGqlApiRequest() {
+export type GqlApiRequestFn = <T, V extends Variables>(
+  document: TypedDocumentNode<T, V>,
+  variables: V,
+) => fp.taskEither.TaskEither<ApiGraphQLError, T>;
+
+export function useMakeGqlApiRequest(): GqlApiRequestFn {
   const store = useApiStore();
 
-  return <T, V extends Variables>(
-    document: TypedDocumentNode<T, V>,
-    variables: V,
-  ): fp.taskEither.TaskEither<ApiGraphQLError, T> => {
+  return (document, variables) => {
     return fp.function.pipe(
       gqlRequest({ client: store.client, document, variables }),
       fp.taskEither.mapLeft(e => new ApiGraphQLError(e.response, e.request)),
