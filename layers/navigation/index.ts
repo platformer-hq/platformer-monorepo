595f452df7ede914dca3a3bfb8b297be9f7c1e21
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-15 22:14:51 +0500

    feat: implement useParsedQuery

--- layers/navigation/index.ts
+++ layers/navigation/index.ts
@@ -1,4 +1,5 @@
 export { default as NavigationStateProvider } from './app/components/NavigationStateProvider.vue';
 export { useNavigationDirection } from './app/composables/useNavigationDirection';
+export { useParsedQuery } from './app/composables/useParsedQuery';
 export { isPageReload } from './app/utils/isPageReload';
 export type { RoutingDirection } from './app/routing-state';
