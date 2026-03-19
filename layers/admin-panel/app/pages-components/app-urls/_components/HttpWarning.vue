1531ed57efdadfc01f9162f0de3b3da644f061e2
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-19 14:04:29 +0500

    feat: implement UrlHttpWarning and reuse in app urls page

--- layers/admin-panel/app/pages-components/app-urls/_components/HttpWarning.vue
+++ layers/admin-panel/app/pages-components/app-urls/_components/HttpWarning.vue
@@ -1,59 +1,14 @@
 <script setup lang="ts">
 defineProps<{ show?: boolean }>();
 
-const { b, e } = bem('app-urls-page-http-warning');
-
-const { t } = useI18n({
-  messages: {
-    en: {
-      title: 'HTTP URL specified',
-      text: 'HTTP links work with a limited number of clients. Use them only during development',
-    },
-    ru: {
-      title: 'Указана HTTP-ссылка',
-      text: 'HTTP-ссылки работают в ограниченном количестве клиентов Telegram. Используйте их только в целях разработки',
-    },
-  },
-});
 const transition = createReversibleTransition({
-  animatedProperties: {
-    opacity: [0, 1],
-    transform: ['scale(0.98)', 'scale(1)'],
-  },
-  animationOptions: {
-    duration: 300,
-    easing: 'ease-out',
-  },
+  animatedProperties: { opacity: [0, 1], transform: ['scale(0.98)', 'scale(1)'] },
+  animationOptions: { duration: 300, easing: 'ease-out' },
 });
 </script>
 
 <template>
   <Transition v-bind="transition" :css="false">
-    <AutoRoundedPanel v-if="show" :class="b()">
-      <AutoTypography :class="e('title')" variant="body" weight="semibold">
-        <IconExclamationMarkTriangleFill28 :class="e('icon')" :size="20"/>
-        {{ t('title') }}
-      </AutoTypography>
-      <AutoTypography>
-        {{ t('text') }}
-      </AutoTypography>
-    </AutoRoundedPanel>
+    <UrlHttpWarning v-if="show" style="margin-top: 16px"/>
   </Transition>
 </template>
-
-<style lang="scss">
-.app-urls-page-http-warning {
-  margin-top: 16px;
-  background-color: #ff9d0a64;
-  padding: 15px 16px;
-
-  &__title {
-    margin-bottom: 6px;
-  }
-
-  &__icon {
-    vertical-align: middle;
-    color: #ff9d0a;
-  }
-}
-</style>
