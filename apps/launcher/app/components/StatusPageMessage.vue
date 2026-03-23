68ccd2666d4ef99e0271a18e7de66e122faafc5b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-24 02:02:28 +0500

    feat: implement StatusPageMessage and StatusPageTitle

---
+++ apps/launcher/app/components/StatusPageMessage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <VTypography class="status-page-message" as="h2" variant="body">
+    <slot/>
+  </VTypography>
+</template>
+
+<style lang="scss">
+.status-page-message {
+  color: var(--subtitle-text-color);
+}
+</style>
