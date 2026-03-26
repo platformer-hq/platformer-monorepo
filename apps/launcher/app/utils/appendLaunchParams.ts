8d3336393f85ec42b092c739a661d373efe222f4
qbnk <wolfram.deus@gmail.com>
2026-03-26 22:34:08 +0500

    fix(appendLaunchParams): add launch parameters to query params, not hash

--- apps/launcher/app/utils/appendLaunchParams.ts
+++ apps/launcher/app/utils/appendLaunchParams.ts
@@ -1,9 +1,7 @@
 export function appendLaunchParams(url: string, launchParams: string): string {
   const urlObject = new URL(url);
-  const hashParams = new URLSearchParams(urlObject.hash.slice(1));
   new URLSearchParams(launchParams).forEach((value, key) => {
-    hashParams.set(key, value);
+    urlObject.searchParams.set(key, value);
   });
-  urlObject.hash = '#' + hashParams.toString();
   return urlObject.toString();
 }
