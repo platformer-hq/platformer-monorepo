948078ba12074f590187d1b99bfb423f3db4f543
qbnk <wolfram.deus@gmail.com>
2026-03-26 22:41:37 +0500

    feat(AppLoader): completely rework the component

--- apps/launcher/app/components/AppLoader.vue
+++ apps/launcher/app/components/AppLoader.vue
@@ -6,26 +6,44 @@ import * as v from 'valibot';
 const props = defineProps<{
   appId: number;
   apiBaseUrl: string;
-  fallbackUrl?: string | null;
+  fallbackUrl?: string;
   initTimeout: number;
   loadTimeout: number;
   initDataRaw: string;
   launchParamsRaw: string;
 }>();
 const emit = defineEmits<{
-  // error: [{ kind: 'init-data-missing' }];
   ready: [];
-  warning: [{ kind: 'http-url' }];
+  apiError: [{ error: Error }];
+  apiTimeout: [{ timeout: number }];
+  appHttpUrl: [{ url: string; type: 'error' | 'warning' }];
+  appDataRetrieved: [];
+  appDeviceInaccessible: [];
+  appNotFound: [];
+  appTimeout: [];
+  appError: [];
 }>();
 
+const frameUrl = ref<{ kind: 'original' | 'fallback'; src: string }>();
+
+const isTimeoutError = (e: unknown) => {
+  return FetchError.is(e) && e.cause instanceof AbortSignal;
+};
+const isAppNotFoundError = (e: unknown) => {
+  return ApiError.is(e) && e.data.code === 'ERR_APP_NOT_FOUND';
+};
 const controller = new AbortController();
-const { stop: cleanupTimeout } = useTimeoutFn(() => controller.abort(), () => props.loadTimeout);
+const { stop: cleanupTimeout } = useTimeoutFn(
+  () => controller.abort(),
+  () => props.loadTimeout,
+);
 
 const launcherParamsRawSecured = computed(() => {
   return secureRawLaunchParams(props.launchParamsRaw, props.initDataRaw);
 });
 
-useQuery({
+const { data, isPending, error } = useQuery({
+  staleTime: 0,
   key: () => [props.appId, props.apiBaseUrl, launcherParamsRawSecured.value],
   query({ signal }) {
     return throwifyAnyEither(
@@ -47,29 +65,96 @@ useQuery({
           );
         }),
         fp.taskEither.chainW(json => {
-          if (v.is(
-            v.looseObject({
-              ok: v.literal(false),
-              error: v.looseObject({ code: v.string(), message: v.nullish(v.string()) }),
-            }),
-            json,
-          )) {
+          if (
+            !v.is(v.variant('ok', [
+              v.looseObject({
+                ok: v.literal(false),
+                error: v.looseObject({
+                  code: v.string(),
+                  message: v.nullish(v.string()),
+                }),
+              }),
+              v.looseObject({
+                ok: v.literal(true),
+                data: v.unknown(),
+              }),
+            ]), json)
+          ) {
+            return fp.taskEither.left(new InvalidResponseDataError(json));
+          }
+          if (!json.ok) {
             return fp.taskEither.left(new ApiError(json.error.code, json.error.message));
           }
-          if (v.is(v.looseObject({ ok: v.literal(true), data: v.unknown() }), json)) {
-            if (v.is(v.looseObject({ url: v.nullish(v.string()) }), json.data)) {
-              return fp.taskEither.right(json.data);
-            }
-            return fp.taskEither.left(new InvalidResponseDataError(json.data));
+          if (v.is(v.looseObject({ url: v.nullish(v.string()) }), json.data)) {
+            return fp.taskEither.right(json.data);
           }
-          return fp.taskEither.left(new InvalidResponseDataError(json));
+          return fp.taskEither.left(new InvalidResponseDataError(json.data));
         }),
       ),
     );
   },
+  retry: (failureCount, error) => (
+    // The error should not be a timeout error.
+    !isTimeoutError(error)
+    // The error should not indicate that the app was not found.
+    && !isAppNotFoundError(error)
+    // Not more than 3 retries.
+    && failureCount < 4
+  ),
+});
+
+watch(isPending, isPending => {
+  if (!isPending) {
+    cleanupTimeout();
+  }
+});
+
+watch(data, data => {
+  if (!data) {
+    return;
+  }
+  if (!data.url) {
+    return emit('appDeviceInaccessible');
+  }
+  if (data.url.startsWith('http://')) {
+    let isWeb = true;
+    try {
+      isWeb = window.self !== window.top;
+    } catch {
+      /* none */
+    }
+    return emit('appHttpUrl', {
+      url: data.url,
+      type: isWeb ? 'error' : 'warning',
+    });
+  }
+  frameUrl.value = { kind: 'original', src: data.url };
+});
+
+watchEffect(() => {
+  if (!error.value) {
+    return;
+  }
+  if (isAppNotFoundError(error.value)) {
+    return emit('appNotFound');
+  }
+  if (!props.fallbackUrl) {
+    if (isTimeoutError(error.value)) {
+      return emit('apiTimeout', { timeout: props.loadTimeout });
+    }
+    return emit('apiError', { error: error.value });
+  }
+  frameUrl.value = { kind: 'original', src: props.fallbackUrl };
 });
 </script>
 
 <template>
-  App loader
+  <AppFrame
+    v-if="frameUrl"
+    :init-timeout
+    :src="frameUrl.src"
+    @ready="$emit('ready')"
+    @error="$emit('appError')"
+    @timeout="$emit('appTimeout')"
+  />
 </template>
