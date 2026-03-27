fd1b57aa26850d3f7e90e64fa1f5093b32adc71e
qbnk <wolfram.deus@gmail.com>
2026-03-27 14:07:01 +0500

    refactor(extractLauncherOptions): use fp-ts

--- apps/launcher/app/utils/extractLauncherOptions.ts
+++ apps/launcher/app/utils/extractLauncherOptions.ts
@@ -1,3 +1,4 @@
+import * as fp from 'fp-ts';
 import * as v from 'valibot';
 
 import type { LocationQuery } from '#vue-router';
@@ -9,47 +10,41 @@ function positiveIntFromStr() {
 /**
  * Extracts the launcher options from the window location.
  */
-export function extractLauncherOptions(query: LocationQuery): ({
-  ok: true;
-  options: {
+export function extractLauncherOptions(
+  query: LocationQuery,
+): fp.either.Either<
+  v.ValiError<v.BaseSchema<unknown, unknown, v.BaseIssue<unknown>>>,
+  {
     appId: number;
     apiBaseUrl: string;
     fallbackUrl?: string | null;
     initTimeout: number;
     loadTimeout: number;
-  };
-} | {
-  ok: false;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  error: v.ValiError<any>;
-}) {
-  try {
-    const argsObject = v.parse(
-      v.looseObject({
-        app_id: positiveIntFromStr(),
-        // app_id: v.optional(positiveIntFromStr(), '1'),
-        api_base_url: v.optional(
-          v.string(),
-          import.meta.env.DEV ? '/api/' : 'https://mini-apps.store/api/',
-        ),
-        fallback_url: v.optional(v.string()),
-        init_timeout: v.optional(positiveIntFromStr(), '5000'),
-        load_timeout: v.optional(positiveIntFromStr(), '10000'),
-      }),
-      query,
-    );
-    return {
-      ok: true,
-      options: {
-        appId: argsObject.app_id,
-        apiBaseUrl: argsObject.api_base_url,
-        fallbackUrl: argsObject.fallback_url,
-        initTimeout: argsObject.init_timeout,
-        loadTimeout: argsObject.load_timeout,
-      },
-    };
-  } catch (e) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return { ok: false, error: (e as v.ValiError<any>) };
   }
+> {
+  const parseResult = v.safeParse(
+    v.looseObject({
+      app_id: positiveIntFromStr(),
+      // app_id: v.optional(positiveIntFromStr(), '1'),
+      api_base_url: v.optional(
+        v.string(),
+        import.meta.env.DEV ? '/api/' : 'https://mini-apps.store/api/',
+      ),
+      fallback_url: v.optional(v.string()),
+      init_timeout: v.optional(positiveIntFromStr(), '5000'),
+      load_timeout: v.optional(positiveIntFromStr(), '10000'),
+    }),
+    query,
+  );
+  if (!parseResult.success) {
+    return fp.either.left(new v.ValiError(parseResult.issues));
+  }
+  const { output } = parseResult;
+  return fp.either.right({
+    appId: output.app_id,
+    apiBaseUrl: new URL(output.api_base_url, window.location.origin).toString(),
+    fallbackUrl: output.fallback_url,
+    initTimeout: output.init_timeout,
+    loadTimeout: output.load_timeout,
+  });
 }
