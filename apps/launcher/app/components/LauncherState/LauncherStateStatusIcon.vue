73f3d3d636ba7e99c110039e7ca2e110c5f2f919
qbnk <wolfram.deus@gmail.com>
2026-03-27 15:31:46 +0500

    feat(LauncherStateStatusIcon): use better animation

--- apps/launcher/app/components/LauncherState/LauncherStateStatusIcon.vue
+++ apps/launcher/app/components/LauncherState/LauncherStateStatusIcon.vue
@@ -3,33 +3,33 @@ defineProps<{
   status: 'error' | 'warning' | 'loading';
 }>();
 const { b, e } = bem('launcher-state-status-icon');
+
+const transition = createReversibleTransition({
+  animatedProperties: {
+    transform: [0.75, 1.1, 1].map(scale => `translate3d(-75%, -75%, 0) scale(${scale})`),
+    opacity: [0, 1],
+  },
+  animationOptions: { duration: 400, easing: 'ease-in-out', fill: 'forwards' },
+});
 </script>
 
 <template>
-  <IconXmark28 v-if="status === 'error'" :class="b('error')"/>
-  <IconExclamationMarkTriangleFill28 v-else-if="status === 'warning'" :class="b('warning')"/>
-  <div v-else-if="status === 'loading'" :class="b('loading')">
-    <LoadingIndicatorIos :class="e('loading-indicator', 'ios')" :size="14"/>
-    <LoadingIndicatorAndroid :class="e('loading-indicator', 'android')" :size="14"/>
-  </div>
+  <Transition v-bind="transition" :css="false">
+    <div v-if="status === 'error'" :class="b('error')">
+      <IconXmark28 :size="16"/>
+    </div>
+    <div v-else-if="status === 'warning'" :class="b('warning')">
+      <IconExclamationMarkTriangleFill28 :size="16"/>
+    </div>
+    <div v-else-if="status === 'loading'" :class="b('loading')">
+      <LoadingIndicatorIos :class="e('loading-indicator', 'ios')" :size="14"/>
+      <LoadingIndicatorAndroid :class="e('loading-indicator', 'android')" :size="14"/>
+    </div>
+  </Transition>
 </template>
 
 <style lang="scss">
 .launcher-state-status-icon {
-  $transform: translate3d(-75%, -75%, 0);
-  @keyframes status-page-error-icon-appear {
-    from {
-      opacity: 0;
-      transform: $transform scale(.65);
-    }
-    50% {
-      transform: $transform scale(1.2);
-    }
-    to {
-      opacity: 1;
-      transform: $transform scale(1);
-    }
-  }
   position: absolute;
   top: 100%;
   left: 100%;
@@ -38,11 +38,10 @@ const { b, e } = bem('launcher-state-status-icon');
   justify-content: center;
   width: 16px;
   height: 16px;
-  padding: 4px;
+  padding: 6px;
   box-sizing: content-box;
   border-radius: 50%;
   color: white;
-  animation: status-page-error-icon-appear 400ms ease forwards;
 
   &--error {
     background: var(--destructive-text-color);
