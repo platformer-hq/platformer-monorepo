8dddcba4088b598e7c270f6907a9e00830e41fb1
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-03-29 21:52:01 +0500

    fix(appendLaunchParams): pass launch params to hash

--- apps/launcher/app/utils/appendLaunchParams.ts
+++ apps/launcher/app/utils/appendLaunchParams.ts
@@ -1,7 +1,9 @@
 export function appendLaunchParams(url: string, launchParams: string): string {
   const urlObject = new URL(url);
+  const hashObj = new URLSearchParams(urlObject.hash.slice(1));
   new URLSearchParams(launchParams).forEach((value, key) => {
-    urlObject.searchParams.set(key, value);
+    hashObj.set(key, value);
   });
+  urlObject.hash = '#' + hashObj.toString();
   return urlObject.toString();
 }
