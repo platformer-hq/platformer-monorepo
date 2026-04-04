a4689ec238cfe6445fbd869f1b06099e2d300eb6
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-04-05 01:54:16 +0500

    feat(utils): implement createProviderTuple

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './camelToKebab.js';
+export * from './createProviderTuple.js';
 export * from './formatNumber.js';
 export * from './throwify.js';
 export * from './throwifyAnyEither.js';
