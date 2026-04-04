5b1eeb8d419d12b09b3ad2659fca94ffc0041d0b
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-04-04 19:25:39 +0500

    chore: remove ui-kit layer

--- nuxt-layers/ui-kit/app/components/List/ListAndroid/ListAndroidItemBodyLeftInput.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <label class="list-android-item-body-input">
-    <slot/>
-  </label>
-</template>
--- nuxt-layers/ui-kit/app/components/List/ListIos/provider.ts
+++
@@ -1,5 +0,0 @@
-import type { ComputedRef } from 'vue';
-
-export const [provideListItemOptions, injectListItemOptions] = createProviderTuple<{
-  large: ComputedRef<boolean>;
-}>();
