8dce9cafab16ad1660780da9d913f8706c892ebf
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-05-16 03:02:44 +0500

    feat(AutoListItemBodyLeftInputElement): reimplement the component

--- apps/admin/layers/auto-components/app/components/AutoListItemBodyLeftInputElement.ts
+++
@@ -1,9 +0,0 @@
-import {
-  ListAndroidItemBodyLeftInputElement,
-  ListIosItemBodyLeftInputElement,
-} from '@tma.js/vue-kit';
-
-export default platformed({
-  fallback: ListAndroidItemBodyLeftInputElement,
-  ios: ListIosItemBodyLeftInputElement,
-});
---
+++ apps/admin/layers/auto-components/app/components/AutoListItemBodyLeftInputElement.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ListIosItemBodyLeftInputElement, ListAndroidItemBodyLeftInputElement } from '@tma.js/vue-kit';
+import { reactivePick } from '@vueuse/core';
+
+const props = withDefaults(defineProps<{
+  multiline?: boolean;
+  /**
+   * True if the element should be scrollable into view when focused. This is sometimes required
+   * for Telegram for iOS when the input is being focused and the keyboard is shown, hiding
+   * the input itself.
+   */
+  scrollIntoViewOnFocus?: boolean;
+  /**
+   * True if the element should blur on click outside.
+   * @default true
+   */
+  blurOnClickOutside?: boolean;
+}>(), {
+  blurOnClickOutside: true,
+});
+
+const platform = useTmaPlatform();
+const shared = reactivePick(props, ['multiline', 'blurOnClickOutside']);
+</script>
+
+<template>
+  <ListIosItemBodyLeftInputElement
+    v-if="platform.isMappedIos"
+    v-bind="{...shared, scrollIntoViewOnFocus}"
+  >
+    <slot/>
+  </ListIosItemBodyLeftInputElement>
+  <ListAndroidItemBodyLeftInputElement v-else v-bind="shared">
+    <slot/>
+  </ListAndroidItemBodyLeftInputElement>
+</template>
