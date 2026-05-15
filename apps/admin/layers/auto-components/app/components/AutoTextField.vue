b2e3a115e04243c9a989257db4d0f616d5d1f162
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-05-16 03:07:52 +0500

    feat(admin): add auto components for TextField

---
+++ apps/admin/layers/auto-components/app/components/AutoTextField.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { TextFieldAndroid, TextFieldIos } from '@tma.js/vue-kit';
+
+import type { ColorReferenceAnyColor } from '#colors/utils/utils';
+
+const props = defineProps<{
+  bgColor?: ColorReferenceAnyColor;
+  textColor?: ColorReferenceAnyColor;
+  rounded?: boolean;
+  elevated?: boolean;
+}>();
+defineSlots<{
+  left(): unknown;
+  input(): unknown;
+  right(): unknown;
+}>();
+
+const platform = useTmaPlatform();
+const bgColor = computed(() => colorReference(props.bgColor) || undefined);
+const textColor = computed(() => colorReference(props.textColor) || undefined);
+const slotIds = ['left', 'input', 'right'] as const;
+</script>
+
+<template>
+  <component
+    :is="platform.isMappedIos ? TextFieldIos : TextFieldAndroid"
+    :style="platform.isMappedIos ? {
+      '--tgui-text-field-ios-bg-color': bgColor,
+      '--tgui-text-field-ios-text-color': textColor,
+    } : {
+      '--tgui-text-field-android-bg-color': bgColor,
+      '--tgui-text-field-android-text-color': textColor,
+    }"
+    v-bind="platform.isMappedIos ? { elevated, rounded } : undefined"
+  >
+    <template v-for="id in slotIds" :key="id" #[id]>
+      <slot :name="id"/>
+    </template>
+  </component>
+</template>
---
+++ apps/admin/layers/auto-components/app/components/AutoTextFieldLeft.ts
@@ -0,0 +1,6 @@
+import { TextFieldAndroidLeft, TextFieldIosLeft } from '@tma.js/vue-kit';
+
+export default platformed({
+  fallback: TextFieldAndroidLeft,
+  ios: TextFieldIosLeft,
+});
