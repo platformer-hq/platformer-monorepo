46f40a8343c9c20a2f27a37817200bcb3e6cf9f3
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-06-15 01:04:58 +0500

    feat(PageNames): register AppServerlessFunction and AppServerlessFunctions

--- apps/admin/layers/pages/app/utils/PageNames.ts
+++ apps/admin/layers/pages/app/utils/PageNames.ts
@@ -19,6 +19,8 @@ export enum PageNames {
   AppUrlViewer = 'app-url-viewer',
   AppCache = 'app-cache',
   AppSplashScreen = 'app-splash-screen',
+  AppServerlessFunction = 'app-serverless-function',
+  AppServerlessFunctions = 'app-serverless-functions',
   AppSplashScreenUpload = 'app-splash-screen-upload',
   CreateApp = 'create-app',
   Account = 'account',
