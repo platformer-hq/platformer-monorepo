2daff6bc058cfa5f791c08652f8ad4dfab82c012
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-06-27 21:43:28 +0500

    feat(useMutationEnhanced): export MutationEnhancedContext

--- apps/admin/layers/api/app/composables/useMutationEnhanced.ts
+++ apps/admin/layers/api/app/composables/useMutationEnhanced.ts
@@ -1,11 +1,11 @@
 import type { UseMutationOptions } from '@pinia/colada';
 
-interface EnhancedContext {
+export interface MutationEnhancedContext {
   apiGqlRequest: ApiGqlRequestFn;
 }
 
 type OverrideContext<TContext extends Record<never, never>> =
-  Omit<TContext, keyof EnhancedContext> & EnhancedContext;
+  Omit<TContext, keyof MutationEnhancedContext> & MutationEnhancedContext;
 
 export type UseMutationEnhancedOptions<
   TData,
@@ -29,7 +29,7 @@ export function useMutationEnhanced<
   return useMutation<TData, TVars, TError, OverrideContext<TContext>>({
     ...options,
     onMutate(vars, context) {
-      const additionalContext: EnhancedContext = { apiGqlRequest };
+      const additionalContext: MutationEnhancedContext = { apiGqlRequest };
       return options.onMutate?.(vars, {
         ...context,
         ...additionalContext,
