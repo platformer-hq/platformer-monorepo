99cedb3fbba9ef5250c5c72d7730914b3c2778fd
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-14 23:56:27 +0500

    feat(apiGqlRequest): mark client as optional

--- packages/api/src/apiGqlRequest.ts
+++ packages/api/src/apiGqlRequest.ts
@@ -14,7 +14,7 @@ export function apiGqlRequest<TData, TVars extends Variables, TError = TypeError
   document,
   variables,
 }: {
-  client: GraphQLClient;
+  client?: GraphQLClient;
   document: TypedDocumentNode<TData, TVars>;
   variables: TVars;
 }): fp.taskEither.TaskEither<ApiGraphQLResponseError | TError, TData> {
