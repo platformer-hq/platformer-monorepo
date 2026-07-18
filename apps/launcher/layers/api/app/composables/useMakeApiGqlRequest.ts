1732bdc85a3b09429902ecf5c0e1c5bebd58e1bb
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-18 22:27:41 +0500

    refactor(useMakeApiGqlRequest): reuse useApiStore

--- apps/launcher/layers/api/app/composables/useMakeApiGqlRequest.ts
+++ apps/launcher/layers/api/app/composables/useMakeApiGqlRequest.ts
@@ -1,18 +1,5 @@
-import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
-import { apiGqlRequest, type ApiGraphQLResponseError } from '@workspace/api';
-import type * as fp from 'fp-ts';
-import type { Variables } from 'graphql-request';
-import type { FetchError } from 'ofetch';
+import type { GqlRequestFn } from '../stores/useApiStore';
 
-export type ApiGqlRequestFn = <TData, TVars extends Variables>(
-  document: TypedDocumentNode<TData, TVars>,
-  variables: TVars,
-) => fp.taskEither.TaskEither<ApiGraphQLResponseError | FetchError, TData>;
-
-export function useMakeApiGqlRequest(): ApiGqlRequestFn {
-  const { gqlClient } = useApiStore();
-
-  return (document, variables) => {
-    return apiGqlRequest({ client: gqlClient, document, variables });
-  };
+export function useMakeApiGqlRequest(): GqlRequestFn {
+  return useApiStore().apiGqlRequest;
 }
