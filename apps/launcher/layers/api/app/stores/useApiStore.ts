36a7f8593a1e6c68265270b288e1afb63763166a
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-18 22:27:23 +0500

    feat(useApiStore): return apiGqlRequest

--- apps/launcher/layers/api/app/stores/useApiStore.ts
+++ apps/launcher/layers/api/app/stores/useApiStore.ts
@@ -1,27 +1,42 @@
-import { GraphQLClient } from 'graphql-request';
+import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { apiGqlRequest, type ApiGraphQLResponseError } from '@workspace/api';
+import type * as fp from 'fp-ts';
+import { GraphQLClient, type Variables } from 'graphql-request';
 import { getRequestURL } from 'h3';
+
+export type GqlRequestFn = <TData, TVars extends Variables>(
+  document: TypedDocumentNode<TData, TVars>,
+  variables: TVars,
+) => fp.taskEither.TaskEither<ApiGraphQLResponseError | FetchError, TData>;
+
+function createMakeGqlRequest(client: GraphQLClient): GqlRequestFn {
+  return (document, variables) => {
+    return apiGqlRequest({ client, document, variables });
+  };
+}
 
 export const useApiStore = defineStore('api', () => {
   const requestEvent = useRequestEvent();
   const config = useRuntimeConfig();
+  const gqlClient = new GraphQLClient(
+    new URL(
+      config.public.gqlApiBaseUrl,
+      requestEvent ? getRequestURL(requestEvent)?.origin : window.location.origin,
+    ).toString(),
+    {
+      requestMiddleware(request) {
+        const url = new URL(request.url);
+        if (request.operationName) {
+          url.searchParams.set('operation', request.operationName);
+        }
+        request.url = url.toString();
+        return request;
+      },
+    },
+  );
+
   return {
-    gqlClient: markRaw(
-      new GraphQLClient(
-        new URL(
-          config.public.gqlApiBaseUrl,
-          requestEvent ? getRequestURL(requestEvent)?.origin : window.location.origin,
-        ).toString(),
-        {
-          requestMiddleware(request) {
-            const url = new URL(request.url);
-            if (request.operationName) {
-              url.searchParams.set('operation', request.operationName);
-            }
-            request.url = url.toString();
-            return request;
-          },
-        },
-      ),
-    ),
+    // gqlClient: markRaw(gqlClient),
+    apiGqlRequest: createMakeGqlRequest(gqlClient),
   };
 });
