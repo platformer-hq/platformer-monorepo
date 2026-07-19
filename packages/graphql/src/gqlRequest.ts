87eabaffeae167cc261f268950b47cc792e594de
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-19 19:43:46 +0500

    fix(graphql): use window.setTimeout instead of setTimeout

--- packages/graphql/src/gqlRequest.ts
+++ packages/graphql/src/gqlRequest.ts
@@ -33,7 +33,7 @@ export function gqlRequest<TData, TVars extends Variables, TError = TypeError>(
   const controller = new AbortController();
   let timeoutId: number | undefined;
   if (timeout) {
-    timeoutId = setTimeout(() => {
+    timeoutId = window.setTimeout(() => {
       controller.abort(new TimeoutError(timeout));
     }, timeout);
   }
