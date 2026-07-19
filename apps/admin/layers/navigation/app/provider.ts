5535d5b3432ca626a323b0ed4b14c8e4cf1232c6
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-19 19:40:02 +0500

    refactor(admin): remove all mentions of @tma.js/vue-kit

--- apps/admin/layers/navigation/app/provider.ts
+++ apps/admin/layers/navigation/app/provider.ts
@@ -1,5 +1,6 @@
-import type { UsePageTransitionState } from '@tma.js/vue-kit';
 import type { DeepReadonly } from 'vue';
+
+import type { UsePageTransitionState } from '~ui-kit/composables/usePageTransition';
 
 export const { provide: providePageTransition, inject: injectPageTransition } = createProvider<{
   state: DeepReadonly<ComputedRef<UsePageTransitionState>>;
