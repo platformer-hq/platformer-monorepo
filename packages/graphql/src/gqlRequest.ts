a9208ad4441d4f23d1005aa1ff6d961a0b1bb0e1
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-19 15:24:45 +0500

    feat(gqlRequest): add signal and timeout options

--- packages/graphql/src/gqlRequest.ts
+++ packages/graphql/src/gqlRequest.ts
@@ -1,4 +1,5 @@
 import type { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { TimeoutError } from '@workspace/errors';
 import * as fp from 'fp-ts';
 import {
   request,
@@ -8,25 +9,48 @@ import {
   type Variables,
 } from 'graphql-request';
 
+export interface GqlRequestOptions<TData, TVars extends Variables> {
+  client?: GraphQLClient;
+  document: TypedDocumentNode<TData, TVars>;
+  variables: TVars;
+  timeout?: number;
+  signal?: AbortSignal;
+}
+
 /**
  * Performs a GraphQL request using specified client.
  * @returns TaskEither with the error and execution result.
  */
-export function gqlRequest<TData, TVars extends Variables, TError = TypeError>({
-  client,
-  document,
-  variables,
-}: {
-  client?: GraphQLClient;
-  document: TypedDocumentNode<TData, TVars>;
-  variables: TVars;
-}): fp.taskEither.TaskEither<ClientError | TError, TData> {
+export function gqlRequest<TData, TVars extends Variables, TError = TypeError>(
+  {
+    client,
+    document,
+    variables,
+    timeout,
+    signal,
+  }: GqlRequestOptions<TData, TVars>,
+): fp.taskEither.TaskEither<ClientError | TError, TData> {
+  const controller = new AbortController();
+  let timeoutId: number | undefined;
+  if (timeout) {
+    timeoutId = setTimeout(() => {
+      controller.abort(new TimeoutError(timeout));
+    }, timeout);
+  }
+  if (signal) {
+    signal.onabort = () => {
+      controller.abort(signal.reason);
+    };
+  }
   // TODO: Add "retry" option.
   return fp.taskEither.tryCatch(() => {
     const options = {
       document,
       variables,
+      signal: controller.signal,
     } as unknown as RequestExtendedOptions<TVars, TData>;
-    return client ? client.request(options) : request(options);
+    return (client ? client.request(options) : request(options)).finally(() => {
+      clearTimeout(timeoutId);
+    });
   }, e => e as ClientError | TError);
 }
