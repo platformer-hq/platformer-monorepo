3a5928fe69be58bf6416fb35780c86fe0a76fb7f
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2026-07-19 19:38:43 +0500

    refactor(launcher/comms): rework types

--- apps/launcher/layers/comms/app/types.ts
+++ apps/launcher/layers/comms/app/types.ts
@@ -8,33 +8,28 @@ type CreateMethod<TParams, TSuccessPayload = never, TErrorPayload = never> = {
 };
 
 /**
- * Prefixed method name.
+ * Method name.
  */
 export type MethodName = keyof MethodsMap;
 
 /**
- * Prefixed method parameters.
+ * Method parameters.
  */
 export type MethodParams<M extends MethodName> = MethodsMap[M]['params'];
-
-/**
- * Unprefixed method name.
- */
-export type UnprefixedMethodName = MethodName extends `platformer:${infer U}` ? U : never;
-
-/**
- * Unprefixed method parameters.
- */
-export type UnprefixedMethodParams<M extends UnprefixedMethodName> = MethodsMap[`platformer:${M}`]['params'];
 
 /**
  * An error that may occur calling any method.
  */
 export type CommonErrorObj = (
-  | { error: 'unknown-method'; name: string }
-  | { error: 'request-id-missing' }
-  | { error: 'params-malformed'; message: string }
+  | { kind: 'unknown-method'; methodName: string }
+  | { kind: 'request-id-missing' }
+  | { kind: 'params-malformed'; issues: string[] }
 );
+
+export interface SpecificErrorObj<TErrorPayload> {
+  kind: 'execution';
+  error: TErrorPayload;
+}
 
 export interface MethodsMap {
   /**
@@ -61,3 +56,13 @@ export interface MethodsMap {
     error: string;
   }>;
 }
+
+export interface ErrorResponse<TError> {
+  fromPlatformer: true;
+  requestId?: string;
+  error: TError;
+}
+
+export type MethodErrorResponseEvent<TMethodName extends MethodName> = SpecificErrorObj<
+  MethodsMap[TMethodName]['errorPayload']
+>;
