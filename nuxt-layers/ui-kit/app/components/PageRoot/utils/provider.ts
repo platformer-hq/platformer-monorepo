380b853c10ab95c4c4db24a3f6e7f529e372b4d0
Vladislav Kibenko <wolfram.deus@gmail.com>
2026-08-02 01:47:00 +0500

    fix(ui-kit/PageRoot): pass a value when calling createProvide

--- nuxt-layers/ui-kit/app/components/PageRoot/utils/provider.ts
+++ nuxt-layers/ui-kit/app/components/PageRoot/utils/provider.ts
@@ -1,6 +1,6 @@
 import type { PageRootExpose } from '../_types';
 
 export const {
-  provide: providePageRootContext,
-  inject: injectPageRootContext,
-} = createProvider<PageRootExpose>();
+  provide: providePageRoot,
+  inject: injectPageRoot,
+} = createProvide<PageRootExpose>('page-root');
