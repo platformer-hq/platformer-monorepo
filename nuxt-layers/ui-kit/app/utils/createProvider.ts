92e14fe1650d2591e0bc80e5a5b2a3fef52a2e7f
Vladislav Kibenko <wolfram.deus@gmail.com>
2026-08-01 16:10:22 +0500

    fix(ui-kit): reimplement inject in createProvider

--- nuxt-layers/ui-kit/app/utils/createProvider.ts
+++ nuxt-layers/ui-kit/app/utils/createProvider.ts
@@ -7,16 +7,17 @@ import type { InjectionKey } from 'vue';
 /* @__NO_SIDE_EFFECTS__ */
 export function createProvider<V>() {
   const k = Symbol() as InjectionKey<V>;
+
+  function inject(): V | undefined;
+  function inject(defaultValue: V): V;
+  function inject(defaultValue?: V): V | undefined {
+    return injectLocal(k, defaultValue);
+  }
+
   return {
     provide(v: V) {
       provideLocal(k, v);
     },
-    inject(): V {
-      const injected = injectLocal(k);
-      if (injected === undefined) {
-        throw new Error('Value was not provided');
-      }
-      return injected;
-    },
+    inject,
   };
 }
